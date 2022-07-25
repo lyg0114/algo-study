09c4365abf93b92b4ec9a68e9f1a55115952a227
yglee <yglee@ksmartpia.com>
2022-07-25 20:15:27 +0900

    make fail test

--- src/test/java/leetCode/TwoSum_1Test.java
+++ src/test/java/leetCode/TwoSum_1Test.java
@@ -1,11 +1,16 @@
 package leetCode;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TwoSum_1Test  {
 
   @Test
   public void test(){
+    int[] nums = {2,7,11,15};
+    Solution solution = new Solution();
+    int[] result = solution.twoSum(nums, 9);
+    assertEquals(result[0] + result[1] ,9);
   }
 
 }
