f8be7e15d4de79178557d30ccdab39fcf6659ecd
yglee <yglee@ksmartpia.com>
2022-07-31 09:25:27 +0900

    - add fail testCode

--- src/main/java/leetCode/topInterView/easy/Array/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/leetCode/topInterView/easy/Array/RemoveDuplicatesFromSortedArray.java
@@ -24,5 +24,8 @@ It does not matter what you leave beyond the returned k (hence they are undersco
  */
 
 public class RemoveDuplicatesFromSortedArray {
-
+  public int removeDuplicates(int[] nums) {
+    return 0;
+  }
 }
+
---
+++ src/test/java/leetCode/topInterView/easy/Array/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,17 @@
+package leetCode.topInterView.easy.Array;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RemoveDuplicatesFromSortedArrayTest {
+
+  @Test
+  public void TESTremoveDuplicates(){
+    int[] nums = {1,1,2};
+    RemoveDuplicatesFromSortedArray rdfs = new RemoveDuplicatesFromSortedArray();
+    int result = rdfs.removeDuplicates(nums);
+    assertEquals(2, result);
+  }
+}
