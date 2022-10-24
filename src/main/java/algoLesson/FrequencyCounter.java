11d0e565ff5be0384932ddb3cce806d3be0cf6b2
lyg0114 <whdnseowkd@naver.com>
2022-10-24 21:23:25 +0900

    FrequencyCounter

---
+++ src/main/java/algoLesson/FrequencyCounter.java
@@ -0,0 +1,16 @@
+package algoLesson;
+
+/**
+ * Write a fucntion called same, which accepts two arrays.
+ * The function should return true if every value in the array has ist's corresponding value
+ * squared in the second array. The frequency of values must be the same
+ *
+ * ex)
+ *  - same([1,2,3], [4,1,9]) // true
+ *  - same([1,2,3], [1,9]) //  false
+ *  - same([1,2,1], [4,4,1]) // false (must be same frequencty)
+ *
+ */
+public class FrequencyCounter {
+
+}
