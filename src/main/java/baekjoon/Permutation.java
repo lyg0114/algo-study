f6dc2ed35377e70ff41dbc2d4fb80f37ee167bc2
lyg0114 <whdnseowkd@naver.com>
2022-10-30 18:29:49 +0900

    add Debug code for Permutation

--- src/main/java/baekjoon/Permutation.java
+++ src/main/java/baekjoon/Permutation.java
@@ -6,6 +6,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+ * calculate nPr
+ * */
 public class Permutation {
 
   private List<Integer> permutations = new ArrayList<Integer>();
@@ -18,35 +21,40 @@ public class Permutation {
 
   public void calculatePermutataion() {
     makeTargetLists();
-    makePermutation(permutations.size(), permutations.size(), 0);
+    makePermutation(permutations.size(), 3, 0);
     System.out.println("##########################");
     System.out.println(permutationCount);
   }
 
   public void makeTargetLists() {
-    int arr[] = {1, 2, 3 , 4};
+    int arr[] = {1, 2, 3};
     permutations = Arrays.stream(arr).boxed().collect(Collectors.toList());
   }
 
-  public void makePermutation(int n, int r, int depth) {
-    if (r == depth) {
-      printPermutations();
+  public void makePermutation(int number, int choice, int depth) {
+    System.out.println("number = " + number + ", choice : " + choice + ", depth : " + depth);
+    if (choice == depth) {
+      printPermutations(depth);
       return;
     }
-
-    for (int i = depth; i < n; i++) {
+    for (int i = depth; i < number; i++) {
+      System.out.println("before [i, depth] : [" + i + "," + depth + "]");
       Collections.swap(permutations, i, depth);
-      makePermutation(n, r, depth + 1);
+      makePermutation(number, choice, depth + 1);
+      System.out.println("after [i, depth] : [" + i + "," + depth + "]");
       Collections.swap(permutations, i, depth);
     }
   }
 
-  private void printPermutations() {
+  private void printPermutations(int depth) {
+    System.out.print("---------------------\n");
     for (Integer permutation : permutations) {
       System.out.print(permutation + " ");
     }
+    if (depth == 3) {
+      System.out.print(" END");
+    }
     permutationCount++;
-    System.out.println("");
-    System.out.println("---------------------");
+    System.out.println("\n---------------------");
   }
 }
