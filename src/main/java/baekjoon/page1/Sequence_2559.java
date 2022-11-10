470656dd8124f84d494fe4e9e206afad7fb3c9b0
lyg0114 <whdnseowkd@naver.com>
2022-11-11 08:11:44 +0900

    Sequence

---
+++ src/main/java/baekjoon/page1/Sequence_2559.java
@@ -0,0 +1,17 @@
+package baekjoon.page1;
+
+import java.util.Scanner;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.page1
+ * @since : 2022/11/11
+ *
+ * 1. 온도값 배열(N)이 주어진다.
+ * 2. 며칠의 값(M)이 주어진다.
+ */
+public class Sequence_2559 {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+  }
+}
