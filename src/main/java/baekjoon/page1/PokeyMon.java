eed6a7b0e98a5cfd932534f479676222209f1939
lyg0114 <whdnseowkd@naver.com>
2022-11-15 22:08:11 +0900

    add pokeyMon

---
+++ src/main/java/baekjoon/page1/PokeyMon.java
@@ -0,0 +1,11 @@
+package baekjoon.page1;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.page1
+ * @since : 2022/11/15
+ */
+public class PokeyMon {
+  public static void main(String[] args){
+  }
+}
