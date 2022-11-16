3f4a5c4f342a2412f9f04916690da5e3a8a2967b
lyg0114 <whdnseowkd@naver.com>
2022-11-17 07:22:10 +0900

    init data

--- src/main/java/baekjoon/page1/PassionKingMIssSin_9375.java
+++ src/main/java/baekjoon/page1/PassionKingMIssSin_9375.java
@@ -1,12 +1,25 @@
 package baekjoon.page1;
 
+import java.util.Scanner;
+
 /**
  * @author : iyeong-gyo
  * @package : baekjoon.page1
  * @since : 2022/11/17
  */
 public class PassionKingMIssSin_9375 {
+
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = Integer.parseInt(scanner.next());
+    int m = Integer.parseInt(scanner.next());
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        String dress = scanner.next();
+        String dressName = scanner.next();
+      }
+    }
 
   }
 }
