32ddb2a1b90faeaf1864fcfb0327587290ebec16
lyg0114 <whdnseowkd@naver.com>
2022-11-17 06:53:05 +0900

    add PassionKingMissSin_9375

---
+++ src/main/java/baekjoon/page1/PassionKingMIssSin_9375.java
@@ -0,0 +1,12 @@
+package baekjoon.page1;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.page1
+ * @since : 2022/11/17
+ */
+public class PassionKingMIssSin_9375 {
+  public static void main(String[] args) {
+
+  }
+}
