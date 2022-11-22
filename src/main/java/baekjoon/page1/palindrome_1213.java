937050089f4712f409dd8df1e48b4ebe2852daae
lyg0114 <whdnseowkd@naver.com>
2022-11-22 19:59:01 +0900

    add palindrome

---
+++ src/main/java/baekjoon/page1/palindrome_1213.java
@@ -0,0 +1,12 @@
+package baekjoon.page1;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.page1
+ * @since : 2022/11/22
+ */
+public class palindrome_1213 {
+  public static void main(String[] ars){
+
+  }
+}
