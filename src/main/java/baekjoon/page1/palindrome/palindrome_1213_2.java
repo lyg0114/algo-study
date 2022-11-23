d4221f2646b31114651beeb166f2eefd73adfcba
lyg0114 <whdnseowkd@naver.com>
2022-11-24 00:12:20 +0900

    complete palindrome

--- src/main/java/baekjoon/page1/palindrome/palindrome_1213_2.java
+++ src/main/java/baekjoon/page1/palindrome/palindrome_1213_2.java
@@ -1,5 +1,8 @@
 package baekjoon.page1.palindrome;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -8,13 +11,37 @@ import java.util.Scanner;
  * @since : 2022/11/23
  */
 public class palindrome_1213_2 {
-
   public static void main(String[] ars) {
     Scanner scan = new Scanner(System.in);
     String inputStr = scan.next();
-    int[] ints = convertStringToCountingArray(inputStr);
+    int[] cnt = convertStringToCountingArray(inputStr);
+    int oddFlag = 0;
+    char mid = 0;
+    String ret = "";
     for (int i = 'Z'; i >= 'A'; i--) {
-      System.out.println((char) i + " : " + ints[i]);
+//      System.out.println((char) i + " : " + ints[i]);
+      if (cnt[i] > 0) {
+        if (cnt[i] % 2 == 1) {
+          mid = (char) i;
+          oddFlag++;
+          cnt[i]--;
+        }
+      }
+      if (oddFlag == 2) {
+        System.out.println("I'm Sorry Hansoo");
+        break;
+      }
+      for (int j = 0; j < cnt[i]; j += 2) {
+        ret = String.valueOf((char) i) + ret;
+        ret += String.valueOf((char) i);
+      }
+    }
+    if (mid > 0) {
+      String before = ret.substring(0, ret.length() / 2);
+      String after = ret.substring(ret.length() / 2, ret.length());
+      System.out.println(before + mid + after);
+    } else {
+      System.out.println(ret);
     }
   }
 
