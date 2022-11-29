5c9105768001ba0b3dc6c6e7d1b4ffd390b0639c
lyg0114 <whdnseowkd@naver.com>
2022-11-30 06:28:52 +0900

    add Multifly

---
+++ src/main/java/baekjoon/page1/Multifly.java
@@ -0,0 +1,12 @@
+package baekjoon.page1;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.page1
+ * @since : 2022/11/30
+ */
+public class Multifly {
+  public static void main(String[] args) {
+
+  }
+}
