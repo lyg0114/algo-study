5021acb217d5323cb3d4440a9dfbba9f784fdd90
lyg0114 <whdnseowkd@naver.com>
2022-12-03 16:09:18 +0900

    need more fix

--- src/main/java/baekjoon/page1/Number1.java
+++ src/main/java/baekjoon/page1/Number1.java
@@ -8,11 +8,24 @@ import java.util.Scanner;
  * @since : 2022/12/03
  */
 public class Number1 {
-  public static void main(String args[]){
+
+  public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int n = Integer.parseInt(scanner.next());
-    if(n%2==0 && n%5==0)
+    if (n % 2 == 0 && n % 5 == 0) {
       return;
+    }
 
+    int ret = calculate(n, 0, 0);
+    System.out.println("ret = " + ret);
+
+  }
+
+  private static int calculate(int n, int i, int ret) {
+    ret =  (ret + (int) Math.pow(10, i));
+    if (ret % n == 0) {
+      return i + 1;
+    }
+    return calculate(n, i + 1, ret);
   }
 }
--- src/test/java/common/CommonTest.java
+++ src/test/java/common/CommonTest.java
@@ -13,4 +13,14 @@ public class CommonTest {
     Collections.sort(strings, String.CASE_INSENSITIVE_ORDER);
     System.out.println("strings.get(0) = " + strings.get(0));
   }
+
+  @Test
+  public void MathPowTest(){
+    int ret = (int) Math.pow(10, 0);
+    System.out.println("ret = " + ret);
+    int ret1 = (int) Math.pow(10, 1);
+    System.out.println("ret1 = " + ret1);
+    int ret2 = (int) Math.pow(10, 2);
+    System.out.println("ret2 = " + ret2);
+  }
 }
