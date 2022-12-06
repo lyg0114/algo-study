561166ce368bfdbf1189911e817314939abf58aa
lyg0114 <whdnseowkd@naver.com>
2022-12-06 23:57:37 +0900

    add 5 vertex

--- src/main/java/baekjoon/AdjacencyMatrix.java
+++ src/main/java/baekjoon/AdjacencyMatrix.java
@@ -12,10 +12,16 @@ public class AdjacencyMatrix {
   public static void main(String[] args) {
     arr[1][2] = 1;
     arr[2][1] = 1;
+
     arr[1][3] = 1;
     arr[3][1] = 1;
+
     arr[3][4] = 1;
     arr[4][3] = 1;
+
+    arr[4][5] = 1;
+    arr[5][4] = 1;
+
 
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++) {
