dd2b50d8ed29daf0b753ed69c1907beb24eea6df
lyg0114 <whdnseowkd@naver.com>
2022-12-06 23:24:11 +0900

    travers grafh using recursive

--- src/main/java/baekjoon/AdjacencyMatrix.java
+++ src/main/java/baekjoon/AdjacencyMatrix.java
@@ -6,40 +6,35 @@ package baekjoon;
  * @since : 2022/12/03
  */
 public class AdjacencyMatrix {
+  private static int[][] arr = new int[10][10];
+  private static int visited[] = new int[10];
 
   public static void main(String[] args) {
-    int arr[][] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-//    arr[1][2] = 1;
-//    arr[2][1] = 1;
-//    arr[1][3] = 1;
-//    arr[3][1] = 1;
-//    arr[3][4] = 1;
-//    arr[4][3] = 1;
+    arr[1][2] = 1;
+    arr[2][1] = 1;
+    arr[1][3] = 1;
+    arr[3][1] = 1;
+    arr[3][4] = 1;
+    arr[4][3] = 1;
 
-    travers(arr, 0, 0);
-
-  }
-
-  private static void travers(int[][] arr, int start1, int start2) {
-    for (int i = start1; i < 10; i++) {
-      for (int j = start2; j < 10; j++) {
-        if (arr[i][j] == 1 && arr[j][i] == 1) {
-          travers(arr, j, i);
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        if (arr[i][j] == 1 && visited[i] == 0) {
+          travers(i);
         }
-        System.out.println("[" + i + "]");
       }
     }
   }
 
+  private static void travers(int from) {
+    visited[from] = 1;
+    System.out.println("current = " + from);
+    for (int i = 0; i < 10; i++) {
+      if(visited[i] == 1) continue;
+      if(arr[from][i]==1){
+        travers(i);
+      }
+    }
+    return;
+  }
 }
