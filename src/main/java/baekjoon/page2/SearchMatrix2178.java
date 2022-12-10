4f18bc46badf38fd61fe5341cce05d5a585bb6e9
lyg0114 <whdnseowkd@naver.com>
2022-12-11 06:08:05 +0900

    add SearchMatrix2178

---
+++ src/main/java/baekjoon/page2/SearchMatrix2178.java
@@ -0,0 +1,12 @@
+package baekjoon.page2;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.page2
+ * @since : 2022/12/11
+ */
+public class SearchMatrix2178 {
+  public static void main(String[] args) {
+
+  }
+}
