d8429fb461b0c77da8fb12015a9924caa52de218
lyg0114 <whdnseowkd@naver.com>
2022-12-11 23:03:45 +0900

    add SearchMatrix

--- src/main/java/baekjoon/page2/SearchMatrix2178.java
+++ src/main/java/baekjoon/page2/SearchMatrix2178.java
@@ -5,6 +5,38 @@ package baekjoon.page2;
  * @package : baekjoon.page2
  * @since : 2022/12/11
  */
+class Position {
+  int x;
+  int y;
+
+  public Position(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+}
+
+class SearchMatrix{
+  private int dx[] = {0, 1, 0, -1};
+  private int dy[] = {-1, 0, 1, 0};
+  private int nx = 0;
+  private int ny = 0;
+
+  private int[][] matirx;
+
+  public void bfs(){
+
+  }
+
+}
+
 public class SearchMatrix2178 {
   public static void main(String[] args) {
 
