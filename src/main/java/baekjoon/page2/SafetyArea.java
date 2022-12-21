8e9f83920617436adca0b9e5b6d16216e39e54b4
lyg0114 <whdnseowkd@naver.com>
2022-12-21 19:09:38 +0900

    seprate class

--- src/main/java/baekjoon/page2/SafetyArea.java
+++ src/main/java/baekjoon/page2/SafetyArea.java
@@ -5,7 +5,7 @@ package baekjoon.page2;
  * @package : baekjoon.page2
  * @since : 2022/12/17
  */
-public class SafetyArea {
+class CalculateSafetyAreaCount {
   private static int dx[] = {0, 1, 0, -1};
   private static int dy[] = {-1, 0, 1, 0};
   private static int[][] matirx = {
@@ -15,8 +15,28 @@ public class SafetyArea {
       {7, 2, 5, 3, 6},
       {8, 9, 5, 2, 7}
   };
+  private static int[][] visited = new int[5][5];
+  private static int nx = 0;
+  private static int ny = 0;
+  private static int ret = 0;
 
+  static void dfs(int x, int y) {
+    visited[x][y] = 1;
+    for (int i = 0; i < 4; i++) {
+      ny = y + dy[i];
+      nx = x + dx[i];
+      if (ny < 0 || nx < 0 || ny >= 5 || nx >= 5) {
+        continue;
+      }
+      if (matirx[nx][ny] == 1 && visited[nx][ny] == 0) {
+        dfs(nx, ny);
+      }
+    }
+    return;
+  }
+}
+
+public class SafetyArea {
   public static void main(String[] args) {
-
   }
 }
