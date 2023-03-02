726c80f90d3a90142d4745a3c6f1a30d308c69ba
lyg0114 <whdnseowkd@naver.com>
2023-03-02 16:11:59 +0900

    add ContainsDuplicate

---
+++ src/main/java/leetCode/topInterView/easy/Array/ContainsDuplicate.java
@@ -0,0 +1,12 @@
+package leetCode.topInterView.easy.Array;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.easy.Array
+ * @since : 2023/03/02
+ * https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/578/
+ */
+public class ContainsDuplicate {
+  public static void main(String[] args) {
+  }
+}
