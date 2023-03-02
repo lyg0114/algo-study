1b8f4d462a42235a9576605c41ab6835de2ea14a
lyg0114 <whdnseowkd@naver.com>
2023-03-02 17:51:10 +0900

    complete SingleNumber

--- src/main/java/leetCode/topInterView/easy/Array/SingleNumber.java
+++ src/main/java/leetCode/topInterView/easy/Array/SingleNumber.java
@@ -1,16 +1,36 @@
 package leetCode.topInterView.easy.Array;
 
+import java.util.HashMap;
+
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.easy.Array
- * @since : 2023/03/02
+ * @since : 2023/03/02 https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/549/
  */
 public class SingleNumber {
 
   public static void main(String[] args) {
-
+    int[] nums = {1};
+    SingleNumberSolution singleNumberSolution = new SingleNumberSolution();
+    System.out.println(singleNumberSolution.singleNumber(nums));
   }
 }
 
 class SingleNumberSolution {
+
+  public int singleNumber(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(nums[i])) {
+        map.replace(nums[i], map.get(nums[i]) + 1);
+      } else {
+        map.put(nums[i], 1);
+      }
+    }
+    for (Integer num : map.keySet()) {
+      if (map.get(num) == 1)
+        return num;
+    }
+    return -1;
+  }
 }
