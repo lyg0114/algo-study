d1b3a51248a670f1bca853e299edda5ef540f68f
lyg0114 <whdnseowkd@naver.com>
2023-03-02 17:09:47 +0900

    ContainsDuplicate finish

--- src/main/java/leetCode/topInterView/easy/Array/ContainsDuplicate.java
+++ src/main/java/leetCode/topInterView/easy/Array/ContainsDuplicate.java
@@ -1,12 +1,29 @@
 package leetCode.topInterView.easy.Array;
+import java.util.HashSet;
 
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.easy.Array
- * @since : 2023/03/02
- * https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/578/
+ * @since : 2023/03/02 https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/578/
  */
 public class ContainsDuplicate {
+
   public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] nums = {1, 2, 3, 1};
+    System.out.println(solution.containsDuplicate(nums));
+  }
+}
+
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> set = new HashSet<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if(set.contains(nums[i])){
+        return true;
+      }
+      set.add(nums[i]);
+    }
+    return false;
   }
 }
--- src/main/java/leetCode/topInterView/easy/Array/RotateArray.java
+++ src/main/java/leetCode/topInterView/easy/Array/RotateArray.java
@@ -6,6 +6,7 @@ package leetCode.topInterView.easy.Array;
  * @since : 2023/03/02 https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
  */
 public class RotateArray {
+
   public static void main(String[] args) {
     int[] nums = {1, 2, 3, 4, 5, 6, 7};
     printArray(nums);
