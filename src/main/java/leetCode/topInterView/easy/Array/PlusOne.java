f74e74e8cf12641e26afdf56ea9b6016e0577620
lyg0114 <whdnseowkd@naver.com>
2023-03-02 19:34:02 +0900

    add PlusOne

---
+++ src/main/java/leetCode/topInterView/easy/Array/PlusOne.java
@@ -0,0 +1,19 @@
+package leetCode.topInterView.easy.Array;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.easy.Array
+ * @since : 2023/03/02
+ */
+public class PlusOne {
+
+  public static void main(String[] args) {
+  }
+
+  class PlusOneSolution {
+
+    public int[] plusOne(int[] digits) {
+      return null;
+    }
+  }
+}
