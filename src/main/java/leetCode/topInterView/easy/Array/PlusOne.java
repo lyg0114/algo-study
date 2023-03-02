2b53d46155e9fe7002cc80b1f8ad0b0dfe1078cd
lyg0114 <whdnseowkd@naver.com>
2023-03-02 21:28:18 +0900

    complete PlusOne

--- src/main/java/leetCode/topInterView/easy/Array/PlusOne.java
+++ src/main/java/leetCode/topInterView/easy/Array/PlusOne.java
@@ -8,12 +8,38 @@ package leetCode.topInterView.easy.Array;
 public class PlusOne {
 
   public static void main(String[] args) {
+    int[] nums = {1, 2, 3, 4};
+    PlusOneSolution solution = new PlusOneSolution();
+    int[] ints = solution.plusOne(nums);
+    for (int anInt : ints) {
+      System.out.print(anInt + ",");
+    }
+  }
+}
+
+class PlusOneSolution {
+
+  public int[] plusOne(int[] digits) {
+    if (digits.length == 1 && digits[0] == 9) {
+      int[] results = {1, 0};
+      return results;
+    }
+    return plusOneCal(digits, digits.length - 1);
   }
 
-  class PlusOneSolution {
-
-    public int[] plusOne(int[] digits) {
-      return null;
+  public int[] plusOneCal(int[] digits, int index) {
+    if (digits[index] == 9 && index != 0) {
+      digits[index] = 0;
+      return plusOneCal(digits, index - 1);
+    } else {
+      digits[index] += 1;
+      if (digits[index] == 10) {
+        int[] results = new int[digits.length + 1];
+        results[0] = 1;
+        results[1] = 0;
+        return results;
+      }
     }
+    return digits;
   }
 }
