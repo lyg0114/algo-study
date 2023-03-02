74dcbd07baa43771b1f541d46a9d454b80735456
lyg0114 <whdnseowkd@naver.com>
2023-03-02 18:49:50 +0900

    complete IntersectionOfTwoArrays

--- src/main/java/leetCode/topInterView/easy/Array/SingleNumber.java
+++ src/main/java/leetCode/topInterView/easy/Array/SingleNumber.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 public class SingleNumber {
 
   public static void main(String[] args) {
-    int[] nums = {1};
+    int[] nums = {1,2,3,4};
     SingleNumberSolution singleNumberSolution = new SingleNumberSolution();
     System.out.println(singleNumberSolution.singleNumber(nums));
   }
