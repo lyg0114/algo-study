7b28ecbf824eb32ac1bca646360fa9979cf123fa
lyg0114 <whdnseowkd@naver.com>
2023-03-03 17:45:46 +0900

    add MoveZeroesMoveZeroesSolution2

---
+++ src/main/java/leetCode/topInterView/easy/Array/moveZero/MoveZeroesMoveZeroesSolution2.java
@@ -0,0 +1,12 @@
+package leetCode.topInterView.easy.Array.moveZero;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.easy.Array.moveZero
+ * @since : 2023/03/03
+ */
+class MoveZeroesMoveZeroesSolution2 implements MoveZeroesSolutionInterface {
+  public void moveZeroes(int[] nums) {
+
+  }
+}
