af8d7038004693503fca60fc5732d24b018268ca
lyg0114 <whdnseowkd@naver.com>
2023-03-07 16:27:02 +0900

    add printMatrix

--- src/main/java/leetCode/topInterView/easy/Array/RotateImage.java
+++ src/main/java/leetCode/topInterView/easy/Array/RotateImage.java
@@ -8,7 +8,25 @@ package leetCode.topInterView.easy.Array;
 public class RotateImage {
 
   public static void main(String[] args) {
+    RotateImageSolution rotateImageSolution = new RotateImageSolution();
+    int[][] matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    rotateImageSolution.rotate(matrix);
 
+    printMatrix(matrix);
+  }
+
+  private static void printMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+//        System.out.print("[" + i + ", " + j + "] : " + matrix[i][j] + " ");
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println("");
+    }
   }
 }
 
