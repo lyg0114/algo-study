974f2160a1523cda834727592e1afdd15e2af9be
lyg0114 <whdnseowkd@naver.com>
2023-03-08 15:05:41 +0900

    clear reverseString

--- src/main/java/leetCode/topInterView/easy/Strings/ReverseString.java
+++ src/main/java/leetCode/topInterView/easy/Strings/ReverseString.java
@@ -8,14 +8,23 @@ package leetCode.topInterView.easy.Strings;
 public class ReverseString {
 
   public static void main(String[] args) {
-    char[] str = new char[100000];
+    char[] str = {'H', 'a', 'n', 'a', 'h'};
     ReverseStringSolution solution = new ReverseStringSolution();
     solution.reverseString(str);
+    for (char c : str) {
+      System.out.print(c);
+    }
   }
 }
 
 class ReverseStringSolution {
 
   public void reverseString(char[] s) {
+    char tmp;
+    for (int i = 0; i < s.length / 2; i++) {
+      tmp = s[i];
+      s[i] = s[s.length - 1 - i];
+      s[s.length - 1 - i] = tmp;
+    }
   }
 }
