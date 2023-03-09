792f558933b0149f81688b51fa45f87b05d8f5eb
lyg0114 <whdnseowkd@naver.com>
2023-03-09 11:30:31 +0900

    add StringtoInteger

---
+++ src/main/java/leetCode/topInterView/easy/Strings/StringtoInteger.java
@@ -0,0 +1,22 @@
+package leetCode.topInterView.easy.Strings;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.easy.Strings
+ * @since : 2023/03/09
+ */
+public class StringtoInteger {
+
+  public static void main(String[] args) {
+    String str = "42";
+    StringtoIntegerSolution solution = new StringtoIntegerSolution();
+    System.out.println(solution.myAtoi(str));
+  }
+}
+
+class StringtoIntegerSolution {
+
+  public int myAtoi(String s) {
+    return 0;
+  }
+}
