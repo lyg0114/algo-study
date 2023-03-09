b472848e9db2cf8f79578684af909e2233f0f792
lyg0114 <whdnseowkd@naver.com>
2023-03-09 09:54:05 +0900

    addValidAnagram

---
+++ src/main/java/leetCode/topInterView/easy/Strings/ValidAnagram.java
@@ -0,0 +1,20 @@
+package leetCode.topInterView.easy.Strings;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.easy.Strings
+ * @since : 2023/03/09
+ */
+public class ValidAnagram {
+
+  public static void main(String[] args) {
+
+  }
+}
+
+class ValidAnagramSolution {
+
+  public boolean isAnagram(String s, String t) {
+    return false;
+  }
+}
