2ccfe83e9349f23895cf553d505273b9b598affa
lyg0114 <whdnseowkd@naver.com>
2023-03-10 17:48:56 +0900

    add checkNegative

--- src/main/java/leetCode/topInterView/easy/Strings/StringtoInteger.java
+++ src/main/java/leetCode/topInterView/easy/Strings/StringtoInteger.java
@@ -3,20 +3,50 @@ package leetCode.topInterView.easy.Strings;
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.easy.Strings
- * @since : 2023/03/09
+ * @since : 2023/03/09 https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/884/
+ * 1. 공백제거 2. +,- 판별 3. 첫번째자리 확인 0이 첫번째에 올 수 없음 4. 자리수 확인
  */
 public class StringtoInteger {
 
   public static void main(String[] args) {
     String str = "42";
     StringtoIntegerSolution solution = new StringtoIntegerSolution();
-    System.out.println(solution.myAtoi(str));
   }
 }
 
 class StringtoIntegerSolution {
 
-  public int myAtoi(String s) {
+  private boolean isNegative = false;
+
+  public int myAtoi(String input) {
+    String trim = input.trim();
+    char[] chars = trim.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (i == 0) {
+        checkNegative(chars[i]);
+      }
+      if (!checkValidation(chars[i])) {
+        return 0;
+      }
+    }
     return 0;
+  }
+
+  private boolean checkValidation(char aChar) {
+    return isNumber(aChar);
+  }
+
+  private boolean isNumber(char aChar) {
+    return aChar > 47 && aChar < 58;
+  }
+
+  private void checkNegative(char aChar) {
+    if (aChar == '-') {
+      isNegative = true;
+    } else if (aChar == '+') {
+      isNegative = false;
+    } else {
+      isNegative = false;
+    }
   }
 }
