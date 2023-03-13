ca2b96302974365aebd84915f1b57eb520c7cda6
lyg0114 <whdnseowkd@naver.com>
2023-03-13 10:30:03 +0900

    create StringtoIntegerSolution2

---
+++ src/main/java/leetCode/topInterView/easy/Strings/atoI/StringtoIntegerSolution2.java
@@ -0,0 +1,13 @@
+package leetCode.topInterView.easy.Strings.atoI;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.easy.Strings.atoI
+ * @since : 2023/03/13
+ */
+public class StringtoIntegerSolution2 implements StringtoIntegerSolution {
+  @Override
+  public int myAtoi(String s) {
+    return 0;
+  }
+}
