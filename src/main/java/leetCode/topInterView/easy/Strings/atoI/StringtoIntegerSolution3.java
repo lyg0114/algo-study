ba77dcc3ff161f23efce034d85ad12d01f5ed0ab
lyg0114 <whdnseowkd@naver.com>
2023-03-14 11:05:32 +0900

    add StringtoIntegerSolution3

--- src/main/java/leetCode/topInterView/easy/Strings/atoI/StringtoIntegerSolution3.java
+++ src/main/java/leetCode/topInterView/easy/Strings/atoI/StringtoIntegerSolution3.java
@@ -1,5 +1,7 @@
 package leetCode.topInterView.easy.Strings.atoI;
 
+import java.util.ArrayList;
+
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.easy.Strings.atoI
@@ -7,8 +9,36 @@ package leetCode.topInterView.easy.Strings.atoI;
  */
 public class StringtoIntegerSolution3 implements StringtoIntegerSolution {
 
-  @Override
+  // TC: O(n)
+  // SC: O(1)
   public int myAtoi(String s) {
-    return 0;
+    if (s == null || s.length() == 0) {
+      return 0;
+    }
+
+    int res = 0;
+    int sign = 1;
+
+    s = s.trim();
+    int idx = 0;
+    int len = s.length();
+
+    if (idx < len && (s.charAt(idx) == '+' || s.charAt(idx) == '-')) {
+      sign = s.charAt(idx) == '+' ? 1 : -1;
+      idx++;
+    }
+
+    while (idx < len && Character.isDigit(s.charAt(idx))) {
+      char ch = s.charAt(idx);
+      if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10
+          && ch - '0' > Integer.MAX_VALUE % 10)) {
+        return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+      }
+
+      res = res * 10 + ch - '0';
+      idx++;
+    }
+
+    return sign * res;
   }
 }
