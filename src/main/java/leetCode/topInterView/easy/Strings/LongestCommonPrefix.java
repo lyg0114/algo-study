73f9d6b266946707b83facffd3f5cf8a6ea04e0b
lyg0114 <whdnseowkd@naver.com>
2023-03-31 22:38:59 +0900

    add LongestCommonPrefix

---
+++ src/main/java/leetCode/topInterView/easy/Strings/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package leetCode.topInterView.easy.Strings;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.easy.Strings
+ * @url : https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/887/
+ * @since : 2023/03/31
+ */
+public class LongestCommonPrefix {
+
+  public static void main(String[] args) {
+
+    LongestCommonPrefixSolution solution = new LongestCommonPrefixSolution();
+  }
+
+}
+
+class LongestCommonPrefixSolution {
+  public String longestCommonPrefix(String[] strs) {
+
+    return null;
+  }
+}
+
