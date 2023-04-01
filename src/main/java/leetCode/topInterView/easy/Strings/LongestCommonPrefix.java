4fd4065fac6f7bf1a6ebaf104848ee25ef49c73b
lyg0114 <whdnseowkd@naver.com>
2023-04-01 11:46:15 +0900

    make bone

--- src/main/java/leetCode/topInterView/easy/Strings/LongestCommonPrefix.java
+++ src/main/java/leetCode/topInterView/easy/Strings/LongestCommonPrefix.java
@@ -1,5 +1,10 @@
 package leetCode.topInterView.easy.Strings;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.easy.Strings
@@ -9,16 +14,30 @@ package leetCode.topInterView.easy.Strings;
 public class LongestCommonPrefix {
 
   public static void main(String[] args) {
-
+    String[] strs = {"12asf34", "asf1234", "asf1", "asf12", "asf123"};
     LongestCommonPrefixSolution solution = new LongestCommonPrefixSolution();
+    solution.longestCommonPrefix(strs);
   }
 
 }
 
 class LongestCommonPrefixSolution {
-  public String longestCommonPrefix(String[] strs) {
 
-    return null;
+  public String longestCommonPrefix(String[] strs) {
+    List<String> collect = Arrays.stream(strs)
+        .sorted((s1, s2) -> s1.length() - s2.length())
+        .collect(Collectors.toList());
+
+    String target = collect.get(0);
+    for (int i = 1; i < collect.size(); i++) {
+      getCommonTarget(target,collect.get(i));
+    }
+
+    return target;
+  }
+
+  private void getCommonTarget(String target, String s) {
+
   }
 }
 
