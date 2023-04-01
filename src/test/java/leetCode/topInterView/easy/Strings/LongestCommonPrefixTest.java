a0a47af61e2d974732636797e7f7742df4ce4ee7
lyg0114 <whdnseowkd@naver.com>
2023-04-01 16:25:13 +0900

    complete String Part

--- src/test/java/leetCode/topInterView/easy/Strings/LongestCommonPrefixTest.java
+++ src/test/java/leetCode/topInterView/easy/Strings/LongestCommonPrefixTest.java
@@ -14,29 +14,36 @@ public class LongestCommonPrefixTest {
   @Test
   public void testCase1() {
     LongestCommonPrefixSolution2 prefix = new LongestCommonPrefixSolution2();
-    String[] strs = {"flow", "xxxflower", "flozzflower", "flower", "flight"};
-    assertEquals("fl", prefix.longestCommonPrefix(strs));
-  }
-
-  @Test
-  public void testCase2() {
-    LongestCommonPrefixSolution2 prefix = new LongestCommonPrefixSolution2();
     String[] strs = {"dog", "racecar", "car"};
     assertEquals("", prefix.longestCommonPrefix(strs));
   }
 
   @Test
-  public void testCase3() {
+  public void testCase2() {
     LongestCommonPrefixSolution2 prefix = new LongestCommonPrefixSolution2();
     String[] strs = {"", "b"};
     assertEquals("", prefix.longestCommonPrefix(strs));
   }
 
   @Test
-  public void testCase4() {
+  public void testCase3() {
     LongestCommonPrefixSolution2 prefix = new LongestCommonPrefixSolution2();
     String[] strs = {"cir", "car"};
     assertEquals("c", prefix.longestCommonPrefix(strs));
   }
 
+  @Test
+  public void testCase4() {
+    LongestCommonPrefixSolution2 prefix = new LongestCommonPrefixSolution2();
+    String[] strs = {"flower", "flow", "flight"};
+    assertEquals("fl", prefix.longestCommonPrefix(strs));
+  }
+
+  @Test
+  public void testIndexOf() {
+    String strs = "xxflower";
+    String target = "flo";
+    System.out.println(strs.indexOf(target));
+    System.out.println(target.indexOf(strs));
+  }
 }
