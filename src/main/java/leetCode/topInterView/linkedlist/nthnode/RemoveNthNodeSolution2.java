f759cfd17a6fabba532c4464d9a756aa8e511912
lyg0114 <whdnseowkd@naver.com>
2023-04-04 23:48:38 +0900

    add RemoveNthNodeSolution2

---
+++ src/main/java/leetCode/topInterView/linkedlist/nthnode/RemoveNthNodeSolution2.java
@@ -0,0 +1,17 @@
+package leetCode.topInterView.linkedlist.nthnode;
+
+import leetCode.topInterView.linkedlist.ListNode;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.linkedlist.nthnode
+ * @since : 2023/04/04
+ */
+class RemoveNthNodeSolution2 implements RemoveNthNodeInterface {
+
+  @Override
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    return null;
+  }
+}
