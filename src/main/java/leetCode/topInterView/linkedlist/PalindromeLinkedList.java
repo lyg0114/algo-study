ac41c2b8d12e4f097b8d29c8650bf7129d72276c
lyg0114 <whdnseowkd@naver.com>
2023-04-05 17:59:46 +0900

    add comment

--- src/main/java/leetCode/topInterView/linkedlist/PalindromeLinkedList.java
+++ src/main/java/leetCode/topInterView/linkedlist/PalindromeLinkedList.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.linkedlist
+ * @url : https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
  * @since : 2023/04/05
  */
 public class PalindromeLinkedList {
