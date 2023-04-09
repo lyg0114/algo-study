c27929b316756dc239ff1cfc967b237b06e67b37
lyg0114 <whdnseowkd@naver.com>
2023-04-09 20:18:10 +0900

    null check

--- src/main/java/leetCode/topInterView/tree/SymmetricTree.java
+++ src/main/java/leetCode/topInterView/tree/SymmetricTree.java
@@ -3,6 +3,7 @@ package leetCode.topInterView.tree;
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.tree
+ * @url :https://leetcode.com/explore/featured/card/top-interview-questions-easy/94/trees/627/
  * @since : 2023/04/09
  */
 public class SymmetricTree {
@@ -14,6 +15,9 @@ public class SymmetricTree {
   private static class SymmetricTreeSolution {
 
     public boolean isSymmetric(TreeNode root) {
+      if(root == null)
+        return false;
+
 
       return false;
     }
