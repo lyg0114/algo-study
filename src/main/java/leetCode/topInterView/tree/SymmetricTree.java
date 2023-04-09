c419a8e554aa491576f941776834035a0bbfc49c
lyg0114 <whdnseowkd@naver.com>
2023-04-09 20:26:40 +0900

    add checkResult

--- src/main/java/leetCode/topInterView/tree/SymmetricTree.java
+++ src/main/java/leetCode/topInterView/tree/SymmetricTree.java
@@ -1,5 +1,7 @@
 package leetCode.topInterView.tree;
 
+import java.util.Stack;
+
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.tree
@@ -15,11 +17,43 @@ public class SymmetricTree {
   private static class SymmetricTreeSolution {
 
     public boolean isSymmetric(TreeNode root) {
-      if(root == null)
+      if (root == null) {
         return false;
+      }
 
+      Stack<Integer> lStack = new Stack<>();
+      Stack<Integer> rStack = new Stack<>();
 
-      return false;
+      leftTraversal(root.left, lStack);
+      rightTraversal(root.right, rStack);
+
+      return checkResult(lStack, rStack);
     }
+
+    private boolean checkResult(Stack<Integer> lStack, Stack<Integer> rStack) {
+      if (lStack.size() != rStack.size()) {
+        return false;
+      }
+      int sSize = lStack.size();
+      for (int i = 0; i < sSize; i++) {
+        Integer l = lStack.pop();
+        Integer r = rStack.pop();
+        if (!l.equals(r)) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    private void rightTraversal(TreeNode right, Stack<Integer> rStack) {
+
+    }
+
+    private void leftTraversal(TreeNode left, Stack<Integer> lStack) {
+
+    }
+
+
   }
 }
