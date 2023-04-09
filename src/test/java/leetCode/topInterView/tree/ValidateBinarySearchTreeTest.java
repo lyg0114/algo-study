41ca2b2a8bdf925d1d2c7e36d432af44bf7dd46b
lyg0114 <whdnseowkd@naver.com>
2023-04-09 20:11:40 +0900

    add test code

--- src/test/java/leetCode/topInterView/tree/ValidateBinarySearchTreeTest.java
+++ src/test/java/leetCode/topInterView/tree/ValidateBinarySearchTreeTest.java
@@ -34,6 +34,43 @@ public class ValidateBinarySearchTreeTest {
     assertTrue(solution.isValidBST(n2));
   }
 
+
+  /*
+        2
+      /   \
+     1     5
+         /   \
+        4     7
+       /     / \
+      3     6   8
+                 \
+                  9
+  */
+  @Test
+  public void testCaseTrue2() {
+    ValidateBinarySearchTreeInter solution = getSolution();
+    TreeNode n1 = new TreeNode(1);
+    TreeNode n2 = new TreeNode(2);
+    TreeNode n3 = new TreeNode(3);
+    TreeNode n4 = new TreeNode(4);
+    TreeNode n5 = new TreeNode(5);
+    TreeNode n6 = new TreeNode(6);
+    TreeNode n7 = new TreeNode(7);
+    TreeNode n8 = new TreeNode(8);
+    TreeNode n9 = new TreeNode(9);
+
+    n2.left = n1;
+    n2.right = n5;
+    n5.left = n4;
+    n5.right = n7;
+    n4.left = n3;
+    n7.left = n6;
+    n7.right = n8;
+    n8.right = n9;
+
+    assertTrue(solution.isValidBST(n2));
+  }
+
   /*
        5
       / \
@@ -56,6 +93,31 @@ public class ValidateBinarySearchTreeTest {
     node4.right = node6;
     assertFalse(solution.isValidBST(node5));
   }
+
+
+  /*
+       3
+      / \
+     1   5
+        / \
+       2   6
+  */
+  @Test
+  public void testCaseFalse2() {
+    ValidateBinarySearchTreeInter solution = getSolution();
+    TreeNode n3 = new TreeNode(3);
+    TreeNode n1 = new TreeNode(1);
+    TreeNode n5 = new TreeNode(5);
+    TreeNode n2 = new TreeNode(2);
+    TreeNode n6 = new TreeNode(6);
+    n3.left = n1;
+    n3.right = n5;
+    n5.left = n2;
+    n5.right = n6;
+    assertFalse(solution.isValidBST(n3));
+  }
+
+
   private ValidateBinarySearchTreeInter getSolution() {
     return new ValidateBinarySearchTreeSolution();
   }
