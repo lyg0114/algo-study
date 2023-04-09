2f683e0cce8fe5c304f20b207d9ab0abe1d16754
lyg0114 <whdnseowkd@naver.com>
2023-04-09 20:13:22 +0900

    add SymmetricTree

---
+++ src/main/java/leetCode/topInterView/tree/SymmetricTree.java
@@ -0,0 +1,21 @@
+package leetCode.topInterView.tree;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.tree
+ * @since : 2023/04/09
+ */
+public class SymmetricTree {
+
+  public static void main(String[] args) {
+
+  }
+
+  private static class SymmetricTreeSolution {
+
+    public boolean isSymmetric(TreeNode root) {
+
+      return false;
+    }
+  }
+}
