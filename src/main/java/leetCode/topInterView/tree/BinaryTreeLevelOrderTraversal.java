5193f9f6bf7865ed19efb12ed1cc530ecaa80c16
lyg0114 <whdnseowkd@naver.com>
2023-04-10 14:47:34 +0900

    add BinaryTreeLevelOrderTraversal

---
+++ src/main/java/leetCode/topInterView/tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,22 @@
+package leetCode.topInterView.tree;
+
+import java.util.List;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.tree
+ * @since : 2023/04/10
+ */
+public class BinaryTreeLevelOrderTraversal {
+
+  public static void main(String[] args) {
+
+  }
+
+  private static class Solution {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+      return null;
+    }
+  }
+}
