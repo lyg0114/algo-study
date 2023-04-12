2f0dde55ad3ad11c548d4780a2d1bb72eac9c7a6
lyg0114 <whdnseowkd@naver.com>
2023-04-12 10:12:44 +0900

    add priority queue test

--- src/test/java/leetCode/topInterView/tree/ConvertSortedArrayToBSTTest.java
+++ src/test/java/leetCode/topInterView/tree/ConvertSortedArrayToBSTTest.java
@@ -1,6 +1,7 @@
 package leetCode.topInterView.tree;
 
-import static org.junit.Assert.*;
+import java.util.PriorityQueue;
+import org.junit.Test;
 
 /**
  * @author : iyeong-gyo
@@ -8,8 +9,22 @@ import static org.junit.Assert.*;
  * @since : 2023/04/12
  */
 public class ConvertSortedArrayToBSTTest {
-  @Test
-  public void testPriorityQueue(){
 
+  @Test
+  public void testPriorityQueue() {
+    PriorityQueue<Integer> integers = new PriorityQueue<>();
+    integers.add(3);
+    integers.add(1);
+    integers.add(2);
+    integers.add(2);
+    integers.add(2);
+
+    System.out.println("integers = " + integers);
+    int size = integers.size();
+    for (int i = 0; i < size; i++) {
+      System.out.println(integers.poll());
+    }
+    System.out.println("#######");
+    System.out.println("integers = " + integers);
   }
 }
