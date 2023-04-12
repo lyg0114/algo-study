8059b6e97566efc5bd8ec6a3bee1579aa1e88668
lyg0114 <whdnseowkd@naver.com>
2023-04-12 11:16:49 +0900

    add comment

--- src/main/java/leetCode/topInterView/tree/ConvertSortedArrayToBST.java
+++ src/main/java/leetCode/topInterView/tree/ConvertSortedArrayToBST.java
@@ -39,8 +39,46 @@ public class ConvertSortedArrayToBST {
     }
   }
 
-  public interface SolutionInterface {
+  public static class Solution1AddComment implements SolutionInterface {
+    // This method takes in an array of integers representing a sorted sequence
+    // and returns a binary search tree (BST) where the root is the middle value
+    // of the array and the left and right subtrees are the BSTs constructed from
+    // the left and right subarrays respectively.
+    public TreeNode sortedArrayToBST(int[] nums) {
+      // Check if the input array is null or empty. If so, return null.
+      if (nums == null || nums.length == 0) {
+        return null;
+      }
+      // Otherwise, call the helper method to construct the BST.
+      return constructBST(nums, 0, nums.length - 1);
+    }
 
+    // This is a recursive helper method that constructs a BST from a given
+    // subarray of nums[start...end]. It works by recursively dividing the
+    // subarray in halves, setting the middle element as the root of the subtree,
+    // and recursively constructing the left and right subtrees.
+    private TreeNode constructBST(int[] nums, int start, int end) {
+      // If the start index is greater than the end index, the subarray is empty
+      // so return null to indicate an empty subtree.
+      if (start > end) {
+        return null;
+      }
+      // Calculate the middle index of the subarray.
+      int mid = start + (end - start) / 2;
+      // Create a new TreeNode with the middle value as its value.
+      TreeNode root = new TreeNode(nums[mid]);
+      // Recursively construct the left subtree with the left subarray
+      // (nums[start...mid-1]).
+      root.left = constructBST(nums, start, mid - 1);
+      // Recursively construct the right subtree with the right subarray
+      // (nums[mid+1...end]).
+      root.right = constructBST(nums, mid + 1, end);
+      // Return the root of the subtree.
+      return root;
+    }
+  }
+
+  public interface SolutionInterface {
     TreeNode sortedArrayToBST(int[] nums);
 
     default void printNodes(TreeNode root) {
