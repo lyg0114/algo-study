701f774db934024d9adb33ad5b07fd522b415ed2
lyg0114 <whdnseowkd@naver.com>
2023-04-17 22:45:09 +0900

    implement : ClimbingStairsSolution

--- src/main/java/leetCode/topInterView/DP/ClimbingStairs.java
+++ src/main/java/leetCode/topInterView/DP/ClimbingStairs.java
@@ -8,14 +8,24 @@ package leetCode.topInterView.DP;
 public class ClimbingStairs {
 
   public static void main(String[] args) {
-
+    ClimbingStairsInterface solution = new ClimbingStairsSolution();
+    solution.climbStairs(4);
   }
 
   private static class ClimbingStairsSolution implements ClimbingStairsInterface {
 
     @Override
     public int climbStairs(int n) {
-      return 0;
+      if (n == 1) {
+        return 1;
+      }
+      int[] dp = new int[n + 1];
+      dp[1] = 1;
+      dp[2] = 2;
+      for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+      }
+      return dp[n];
     }
   }
 
