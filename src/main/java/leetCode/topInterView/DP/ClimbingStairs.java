0a7700937a6d8af3334d2bbcb8842ba8bc3b80cc
lyg0114 <whdnseowkd@naver.com>
2023-04-17 22:57:16 +0900

    add recursive solution for DP

--- src/main/java/leetCode/topInterView/DP/ClimbingStairs.java
+++ src/main/java/leetCode/topInterView/DP/ClimbingStairs.java
@@ -8,11 +8,15 @@ package leetCode.topInterView.DP;
 public class ClimbingStairs {
 
   public static void main(String[] args) {
-    ClimbingStairsInterface solution = new ClimbingStairsSolution();
-    solution.climbStairs(4);
+    ClimbingStairsInterface solution = getSolution();
+    System.out.println(solution.climbStairs(4));
   }
 
-  private static class ClimbingStairsSolution implements ClimbingStairsInterface {
+  private static ClimbingStairsArraySolution getSolution() {
+    return new ClimbingStairsArraySolution();
+  }
+
+  private static class ClimbingStairsArraySolution implements ClimbingStairsInterface {
 
     @Override
     public int climbStairs(int n) {
@@ -26,6 +30,20 @@ public class ClimbingStairs {
         dp[i] = dp[i - 1] + dp[i - 2];
       }
       return dp[n];
+    }
+  }
+
+  private static class ClimbingStairsRecursiveSolution implements ClimbingStairsInterface {
+
+    @Override
+    public int climbStairs(int n) {
+      if (n == 1) {
+        return 1;
+      }
+      if (n == 2) {
+        return 2;
+      }
+      return climbStairs(n - 1) + climbStairs(n - 2);
     }
   }
 
