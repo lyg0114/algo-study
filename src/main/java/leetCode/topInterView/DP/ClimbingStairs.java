ed3b2f7580122d522fea79003df5e2355021d796
lyg0114 <whdnseowkd@naver.com>
2023-04-17 22:36:38 +0900

    add ClimbingStairs

---
+++ src/main/java/leetCode/topInterView/DP/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package leetCode.topInterView.DP;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.DP
+ * @since : 2023/04/17
+ */
+public class ClimbingStairs {
+
+  public static void main(String[] args) {
+
+  }
+
+  private static class ClimbingStairsSolution implements ClimbingStairsInterface {
+
+    @Override
+    public int climbStairs(int n) {
+      return 0;
+    }
+  }
+
+  private interface ClimbingStairsInterface {
+
+    int climbStairs(int n);
+  }
+}
