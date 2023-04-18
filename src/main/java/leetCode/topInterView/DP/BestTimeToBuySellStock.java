2031c574861b5998973c80acd34a229746e8de99
lyg0114 <whdnseowkd@naver.com>
2023-04-18 22:50:24 +0900

    add : BestTimeToBuySellStockSolution

--- src/main/java/leetCode/topInterView/DP/BestTimeToBuySellStock.java
+++ src/main/java/leetCode/topInterView/DP/BestTimeToBuySellStock.java
@@ -7,14 +7,27 @@ package leetCode.topInterView.DP;
  */
 public class BestTimeToBuySellStock {
   public static void main(String[] args) {
-
+    BestTimeToBuySellStockInterface solution = new BestTimeToBuySellStockSolution();
+    int[] prices = {7, 1, 5, 3, 6, 4};
+    int result = solution.maxProfit(prices);
+    System.out.println("result = " + result);
   }
 }
 
 class BestTimeToBuySellStockSolution implements BestTimeToBuySellStockInterface {
   @Override
   public int maxProfit(int[] prices) {
-    return 0;
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+      if (prices[i] < minPrice) {
+        minPrice = prices[i];
+      } else if (prices[i] - minPrice > maxProfit) {
+        maxProfit = prices[i] - minPrice;
+      }
+    }
+    return maxProfit;
   }
 }
 
