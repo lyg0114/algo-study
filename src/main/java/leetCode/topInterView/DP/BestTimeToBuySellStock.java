fdb2607fcaa6341a5178f200be2349b91f89fd96
lyg0114 <whdnseowkd@naver.com>
2023-04-18 22:43:46 +0900

    add BestTimeToBuySellStock

---
+++ src/main/java/leetCode/topInterView/DP/BestTimeToBuySellStock.java
@@ -0,0 +1,23 @@
+package leetCode.topInterView.DP;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.DP
+ * @since : 2023/04/18
+ */
+public class BestTimeToBuySellStock {
+  public static void main(String[] args) {
+
+  }
+}
+
+class BestTimeToBuySellStockSolution implements BestTimeToBuySellStockInterface {
+  @Override
+  public int maxProfit(int[] prices) {
+    return 0;
+  }
+}
+
+interface BestTimeToBuySellStockInterface {
+  int maxProfit(int[] prices);
+}
