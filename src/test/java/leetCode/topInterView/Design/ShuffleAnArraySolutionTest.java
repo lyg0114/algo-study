550848dacf19395ec72708ad7acdb241e55bc2d5
lyg0114 <whdnseowkd@naver.com>
2023-04-20 15:10:19 +0900

    add test : ShuffleAnArraySolutionTest

---
+++ src/test/java/leetCode/topInterView/Design/ShuffleAnArraySolutionTest.java
@@ -0,0 +1,20 @@
+package leetCode.topInterView.Design;
+
+import java.util.Random;
+import org.junit.Test;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.Design
+ * @since : 2023/04/20
+ */
+public class ShuffleAnArraySolutionTest {
+
+  @Test
+  public void makeRandomNumberTest() {
+    Random random = new Random();
+    System.out.println(random.nextInt());
+    System.out.println(random.nextInt());
+    System.out.println(random.nextInt());
+  }
+}
