b543e58277e44eb8f832cc8157e635160188d7fd
lyg0114 <whdnseowkd@naver.com>
2023-04-26 22:21:24 +0900

    refactoring

--- src/main/java/algoLesson/FrequencyCounter.java
+++ src/main/java/algoLesson/FrequencyCounter.java
@@ -7,24 +7,23 @@ import java.util.HashMap;
  * value in the array has ist's corresponding value squared in the second array. The frequency of
  * values must be the same
  * <p>
- * ex)
- * - same([1,2,3], [4,1,9]) //  true
- * - same([1,2,3], [1,9]) //  false
- * - same([1,2,1], [4,4,1]) // false (must be same frequency)
- *
+ * ex) - same([1,2,3], [4,1,9]) //  true - same([1,2,3], [1,9]) //  false - same([1,2,1], [4,4,1])
+ * // false (must be same frequency)
  */
 public class FrequencyCounter {
+
   public static void main(String[] args) {
     FrequencyCounter frequencyCounter = new FrequencyCounter();
-    int arr1[] = {1,2,3,2,5};
-    int arr2[] = {9,1,4,4,11};
+    int arr1[] = {1, 2, 3, 2, 5};
+    int arr2[] = {9, 1, 4, 4, 11};
     boolean same = frequencyCounter.same(arr1, arr2);
     System.out.println("result = " + same);
   }
 
   public boolean same(int arr1[], int arr2[]) {
-    if(arr1.length != arr2.length)
+    if (arr1.length != arr2.length) {
       return false;
+    }
 
     HashMap<Integer, Integer> map1 = new HashMap<Integer, Integer>();
     HashMap<Integer, Integer> map2 = new HashMap<Integer, Integer>();
@@ -33,10 +32,10 @@ public class FrequencyCounter {
 
     for (Integer m1Val : map1.keySet()) {
       Integer squared = m1Val * m1Val;
-      if(map2.get(squared) == null){
+      if (map2.get(squared) == null) {
         return false;
       }
-      if(map2.get(squared) != map1.get(m1Val)){
+      if (map2.get(squared) != map1.get(m1Val)) {
         return false;
       }
     }
