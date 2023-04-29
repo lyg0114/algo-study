5f0aec8d922304da8004ea43e6f34972876d0d66
lyg0114 <whdnseowkd@naver.com>
2023-04-29 21:09:47 +0900

    init : MinStack

---
+++ src/main/java/leetCode/topInterView/Design/MinStack.java
@@ -0,0 +1,50 @@
+package leetCode.topInterView.Design;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.Design
+ * @url : https://leetcode.com/explore/featured/card/top-interview-questions-easy/98/design/562/
+ * @since : 2023/04/29
+ */
+public class MinStack {
+  public static void main(String[] args) {
+
+  }
+}
+
+class MinStackSolution implements MinStackInterface {
+
+  public MinStackSolution() {
+  }
+
+  @Override
+  public void push(int val) {
+
+  }
+
+  @Override
+  public void pop() {
+
+  }
+
+  @Override
+  public int top() {
+    return 0;
+  }
+
+  @Override
+  public int getMin() {
+    return 0;
+  }
+}
+
+interface MinStackInterface {
+
+  void push(int val);
+
+  void pop();
+
+  int top();
+
+  int getMin();
+}
