30db524a4478ad4f2f1d0084467ebea63b103480
lyg0114 <whdnseowkd@naver.com>
2023-05-02 22:47:52 +0900

    Add : CountPrimes class

---
+++ src/main/java/leetCode/topInterView/Math/CountPrimes.java
@@ -0,0 +1,24 @@
+package leetCode.topInterView.Math;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.Math
+ * @url : https://leetcode.com/explore/featured/card/top-interview-questions-easy/102/math/744/
+ * @since : 2023/05/02
+ */
+public class CountPrimes {
+
+  public static class CountPrimesSolution implements CountPrimesInterface {
+
+    @Override
+    public int countPrimes(int n) {
+      return 0;
+    }
+  }
+
+  public interface CountPrimesInterface {
+
+    int countPrimes(int n);
+  }
+
+}
