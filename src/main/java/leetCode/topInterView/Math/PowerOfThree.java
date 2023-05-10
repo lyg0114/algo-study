9928e8ac30ea816f69a27fe607f60b390d21eebe
lyg0114 <whdnseowkd@naver.com>
2023-05-10 19:11:35 +0900

    Add : add PowerOfThree class

---
+++ src/main/java/leetCode/topInterView/Math/PowerOfThree.java
@@ -0,0 +1,24 @@
+package leetCode.topInterView.Math;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.Math
+ * @since : 2023/05/10
+ */
+public class PowerOfThree {
+
+
+  public static class PowerOfThreeSolution implements PowerOfThreeInterface {
+
+    @Override
+    public boolean isPowerOfThree(int n) {
+      return false;
+    }
+  }
+
+
+  public interface PowerOfThreeInterface {
+
+    boolean isPowerOfThree(int n);
+  }
+}
