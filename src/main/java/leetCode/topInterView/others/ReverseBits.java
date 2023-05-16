f494f2849bb109fe2a88784d152e7934e75b63c8
lyg0114 <whdnseowkd@naver.com>
2023-05-16 10:14:50 +0900

    Add : solution ReverseBits

--- src/main/java/leetCode/topInterView/others/ReverseBits.java
+++ src/main/java/leetCode/topInterView/others/ReverseBits.java
@@ -11,15 +11,14 @@ public class ReverseBits {
 
   public static void main(String[] args) {
     ReverseBitsInterface solution = getSolution();
-    solution.reverseBits(20);
+    solution.reverseBits(14968252);
   }
 
   private static ReverseBitsInterface getSolution() {
-    return new ReverseBitsSolution2();
+    return new ReverseBitsSolution3();
   }
 
   public static class ReverseBitsSolution2 implements ReverseBitsInterface {
-
     @Override
     public int reverseBits(int n) {
       int result = 0;
@@ -31,8 +30,25 @@ public class ReverseBits {
     }
   }
 
-  public static class ReverseBitsSolution implements ReverseBitsInterface {
+  public static class ReverseBitsSolution3 implements ReverseBitsInterface {
+    @Override
+    public int reverseBits(int n) {
+      char[] results = Integer.toBinaryString(n).toCharArray();
+      int left = 0;
+      int right = results.length - 1;
+      char tmp;
+      while (left < right) {
+        tmp = results[right];
+        results[right] = results[left];
+        results[left] = tmp;
+        left++;
+        right--;
+      }
+      return Integer.parseInt(new String(results));
+    }
+  }
 
+  public static class ReverseBitsSolution implements ReverseBitsInterface {
     @Override
     public int reverseBits(int n) {
       String s = Integer.toBinaryString(n);
@@ -41,7 +57,6 @@ public class ReverseBits {
       for (int i = 0; i < length; i++) {
         stack.push(s.charAt(i));
       }
-
       StringBuffer sb = new StringBuffer();
       while (!stack.isEmpty()) {
         sb.append(stack.pop());
@@ -51,9 +66,7 @@ public class ReverseBits {
     }
   }
 
-
   interface ReverseBitsInterface {
-
     int reverseBits(int n);
   }
 }
