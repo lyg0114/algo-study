6ffbf05b6e7c597edf645c36cfcb01b5fd27bf90
lyg0114 <whdnseowkd@naver.com>
2023-05-16 11:02:00 +0900

    Add : add PascalsTriangle

--- src/main/java/leetCode/topInterView/others/PascalsTriangle.java
+++ src/main/java/leetCode/topInterView/others/PascalsTriangle.java
@@ -13,7 +13,7 @@ public class PascalsTriangle {
 
   public static void main(String[] args) {
     PascalsTriangleInterface solution = getSolution();
-    solution.generate(3);
+    System.out.println(solution.generate(5));
   }
 
   private static PascalsTriangleInterface getSolution() {
@@ -28,21 +28,38 @@ public class PascalsTriangle {
         return null;
       }
 
-      List<List<Integer>> arrayLists = new ArrayList<>();
+      List<List<Integer>> results = new ArrayList<>();
       if (numRows == 1) {
-        arrayLists.add(Arrays.asList(1));
-        return arrayLists;
+        results.add(Arrays.asList(1));
+        return results;
       }
       if (numRows == 2) {
-        arrayLists.add(Arrays.asList(1,2));
-        return arrayLists;
+        results.add(Arrays.asList(1));
+        results.add(Arrays.asList(1, 1));
+        return results;
       }
 
+      results.add(Arrays.asList(1));
+      results.add(Arrays.asList(1, 1));
       for (int i = 2; i < numRows; i++) {
-
+        ArrayList<Integer> currents = new ArrayList<>();
+        int length = i + 1;
+        for (int j = 0; j < length; j++) {
+          if (j == 0 || j == length - 1) {
+            currents.add(1);
+          } else {
+            int cur = i;
+            cur--;
+            List<Integer> integers1 = results.get(cur);
+            int before = j;
+            before--;
+            int sum = integers1.get(before).intValue() + integers1.get(j).intValue();
+            currents.add(sum);
+          }
+        }
+        results.add(currents);
       }
-
-      return null;
+      return results;
     }
   }
 
