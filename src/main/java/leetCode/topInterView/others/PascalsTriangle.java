c67f14e3de609f9db25f443b624da82124498677
lyg0114 <whdnseowkd@naver.com>
2023-05-16 10:20:31 +0900

    Add : add PascalsTriangle

---
+++ src/main/java/leetCode/topInterView/others/PascalsTriangle.java
@@ -0,0 +1,30 @@
+package leetCode.topInterView.others;
+
+import java.util.List;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.others
+ * @since : 2023/05/16
+ */
+public class PascalsTriangle {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static class PascalsTriangleSolution implements PascalsTriangleInterface {
+
+    @Override
+    public List<List<Integer>> generate(int numRows) {
+      return null;
+    }
+  }
+
+
+  interface PascalsTriangleInterface {
+
+    List<List<Integer>> generate(int numRows);
+  }
+}
+
