72c50679b063b501c808adc0dff5dc232537be5b
lyg0114 <whdnseowkd@naver.com>
2023-05-16 10:40:33 +0900

    Add : validation

--- src/main/java/leetCode/topInterView/others/PascalsTriangle.java
+++ src/main/java/leetCode/topInterView/others/PascalsTriangle.java
@@ -1,5 +1,7 @@
 package leetCode.topInterView.others;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,13 +12,36 @@ import java.util.List;
 public class PascalsTriangle {
 
   public static void main(String[] args) {
+    PascalsTriangleInterface solution = getSolution();
+    solution.generate(3);
+  }
 
+  private static PascalsTriangleInterface getSolution() {
+    return new PascalsTriangleSolution();
   }
 
   public static class PascalsTriangleSolution implements PascalsTriangleInterface {
 
     @Override
     public List<List<Integer>> generate(int numRows) {
+      if (numRows == 0) {
+        return null;
+      }
+
+      List<List<Integer>> arrayLists = new ArrayList<>();
+      if (numRows == 1) {
+        arrayLists.add(Arrays.asList(1));
+        return arrayLists;
+      }
+      if (numRows == 2) {
+        arrayLists.add(Arrays.asList(1,2));
+        return arrayLists;
+      }
+
+      for (int i = 2; i < numRows; i++) {
+
+      }
+
       return null;
     }
   }
