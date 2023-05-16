76c2a541efc956681b363a443d4810e3873b95c1
lyg0114 <whdnseowkd@naver.com>
2023-05-16 17:15:20 +0900

    Add : add ValidParentheses

---
+++ src/main/java/leetCode/topInterView/others/ValidParentheses.java
@@ -0,0 +1,26 @@
+package leetCode.topInterView.others;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.others
+ * @since : 2023/05/16
+ */
+public class ValidParentheses {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static class ValidParenthesesSolution implements ValidParenthesesInterface {
+
+    @Override
+    public boolean isValid(String s) {
+      return false;
+    }
+  }
+
+  interface ValidParenthesesInterface {
+
+    boolean isValid(String s);
+  }
+}
