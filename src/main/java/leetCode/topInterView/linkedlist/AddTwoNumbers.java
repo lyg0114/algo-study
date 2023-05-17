aa71d186c944bbe59f7e7b91602ef8bcdb0f1abc
lyg0114 <whdnseowkd@naver.com>
2023-05-17 09:23:50 +0900

    Add : add AddTwoNumbers

---
+++ src/main/java/leetCode/topInterView/linkedlist/AddTwoNumbers.java
@@ -0,0 +1,32 @@
+package leetCode.topInterView.linkedlist;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.others
+ * @since : 2023/05/17
+ */
+public class AddTwoNumbers {
+
+  public static void main(String[] args) {
+
+    AddTwoNumbersInterface solution = getSolution();
+  }
+
+  private static AddTwoNumbersInterface getSolution() {
+    return null;
+  }
+
+  public static class AddTwoNumbersSolution implements AddTwoNumbersInterface {
+
+    @Override
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+      return null;
+    }
+  }
+
+  interface AddTwoNumbersInterface {
+
+    ListNode addTwoNumbers(ListNode l1, ListNode l2);
+  }
+}
+
