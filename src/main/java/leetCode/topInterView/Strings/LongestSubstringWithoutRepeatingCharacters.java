e333b44222920acf3d8f5b547ee50b65066531b8
lyg0114 <whdnseowkd@naver.com>
2023-05-20 12:11:35 +0900

    Add : LongestSubstringWithoutRepeatingCharacters

---
+++ src/main/java/leetCode/topInterView/Strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,28 @@
+package leetCode.topInterView.Strings;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.Strings
+ * @since : 2023/05/20
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static class LongestSubstringWithoutRepeatingCharactersInterfaceSolution implements
+      LongestSubstringWithoutRepeatingCharactersInterface {
+
+    @Override
+    public int lengthOfLongestSubstring(String s) {
+      return 0;
+    }
+  }
+
+  interface LongestSubstringWithoutRepeatingCharactersInterface {
+
+    int lengthOfLongestSubstring(String s);
+  }
+}
+
