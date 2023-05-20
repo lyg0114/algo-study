05f963d546012e655e1b297cdfc64015ecb805e6
lyg0114 <whdnseowkd@naver.com>
2023-05-20 16:01:23 +0900

    Add LongestSubstringWithoutRepeatingCharactersInterfaceSolutionV1

--- src/main/java/leetCode/topInterView/Strings/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/leetCode/topInterView/Strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,7 @@
 package leetCode.topInterView.Strings;
 
+import java.util.HashMap;
+
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.Strings
@@ -9,22 +11,61 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
   public static void main(String[] args) {
     LongestSubstringWithoutRepeatingCharactersInterface solution = getSolution();
-
-    String s = "abcabcbb";
+    String s = "dvdf";
     int results = solution.lengthOfLongestSubstring(s);
     System.out.println("results = " + results);
   }
 
   public static LongestSubstringWithoutRepeatingCharactersInterface getSolution() {
-    return new LongestSubstringWithoutRepeatingCharactersInterfaceSolution();
+    return new LongestSubstringWithoutRepeatingCharactersInterfaceSolutionV1();
   }
 
-  public static class LongestSubstringWithoutRepeatingCharactersInterfaceSolution implements
+  public static class LongestSubstringWithoutRepeatingCharactersInterfaceSolutionV1 implements
       LongestSubstringWithoutRepeatingCharactersInterface {
 
     @Override
     public int lengthOfLongestSubstring(String s) {
-      return 0;
+      if (s == null) { return 0; }
+      if (s.length() == 1) { return 1; }
+      if (s.length() == 2) {
+        if (s.charAt(0) != s.charAt(1)) {
+          return 2;
+        } else {
+          return 1;
+        }
+      }
+
+      HashMap<Character, Integer> tmpMaps = new HashMap<>();
+      String tmp = "";
+      StringBuffer bf = new StringBuffer();
+      for (int i = 0; i < s.length(); i++) {
+        if (!tmpMaps.containsKey(s.charAt(i))) {
+          tmpMaps.put(s.charAt(i), 1);
+        } else {
+          for (Character ch : tmpMaps.keySet()) {
+            bf.append(ch);
+          }
+          if (tmp.length() < bf.toString().length()) {
+            tmp = bf.toString();
+          }
+          tmpMaps.remove(s.charAt(i));
+          bf.setLength(0);
+          i--;
+        }
+      }
+
+      if (tmpMaps.size() > 0) {
+        bf.setLength(0);
+        for (Character ch : tmpMaps.keySet()) {
+          bf.append(ch);
+        }
+        String s1 = bf.toString();
+        if (s1.length() > tmp.length()) {
+          tmp = s1;
+        }
+      }
+      System.out.println("tmp = " + tmp);
+      return tmp.length();
     }
   }
 
