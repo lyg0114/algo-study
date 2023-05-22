f1a735ac262b2fb4eceb9cb53d74fbb83b08e247
lyg0114 <whdnseowkd@naver.com>
2023-05-22 16:15:53 +0900

    Add : add test code

--- src/test/java/leetCode/topInterView/Strings/LongestPalindromicSubstringTest.java
+++ src/test/java/leetCode/topInterView/Strings/LongestPalindromicSubstringTest.java
@@ -3,6 +3,7 @@ package leetCode.topInterView.Strings;
 import static org.junit.Assert.*;
 
 import leetCode.topInterView.Strings.LongestPalindromicSubstring.LongestPalindromicSubstringInterface;
+import leetCode.topInterView.Strings.LongestPalindromicSubstring.LongestPalindromicSubstringSolutionV2;
 import org.junit.Test;
 
 /**
@@ -13,7 +14,7 @@ import org.junit.Test;
 public class LongestPalindromicSubstringTest {
 
   @Test
-  public void test_1(){
+  public void test_1() {
     String input = "babad";
     LongestPalindromicSubstringInterface solution = LongestPalindromicSubstring.getSolution();
     String result = solution.longestPalindrome(input);
@@ -21,7 +22,7 @@ public class LongestPalindromicSubstringTest {
   }
 
   @Test
-  public void test_2(){
+  public void test_2() {
     String input = "qwefdfrlkjasdf";
     LongestPalindromicSubstringInterface solution = LongestPalindromicSubstring.getSolution();
     String result = solution.longestPalindrome(input);
@@ -29,11 +30,26 @@ public class LongestPalindromicSubstringTest {
   }
 
   @Test
-  public void test_3(){
+  public void test_3() {
     String input = "abba";
     LongestPalindromicSubstringInterface solution = LongestPalindromicSubstring.getSolution();
     String result = solution.longestPalindrome(input);
     System.out.println("result = " + result);
+  }
+
+  @Test
+  public void test_expandAroundCenter_1() {
+    String input = "alsdkdffdjhba";
+    LongestPalindromicSubstringSolutionV2 solution = new LongestPalindromicSubstringSolutionV2();
+    int i = solution.expandAroundCenter(input, 6, 7);
+    System.out.println("i = " + i);
+  }
+
+  @Test
+  public void test_substr(){
+    String test = "123adfda978";
+    String substring = test.substring(0, 2);
+    System.out.println("substring = " + substring);
   }
 
 }
