2d5e6dbedf08d30d083b4286f615ec289010a84e
lyg0114 <whdnseowkd@naver.com>
2023-05-22 11:44:32 +0900

    Add : LongestPalindromicSubstringSolutionV1

--- src/main/java/leetCode/topInterView/Strings/LongestPalindromicSubstring.java
+++ src/main/java/leetCode/topInterView/Strings/LongestPalindromicSubstring.java
@@ -9,21 +9,65 @@ public class LongestPalindromicSubstring {
 
   public static void main(String[] args) {
     LongestPalindromicSubstringInterface solution = getSolution();
-    String input = "babad";
+//    String input = "babad";
+//    String input = "cbbd";
+    String input = "ab";
     String output = solution.longestPalindrome(input);
     System.out.println("output = " + output);
   }
 
   private static LongestPalindromicSubstringInterface getSolution() {
-    return new LongestPalindromicSubstringSolution();
+    return new LongestPalindromicSubstringSolutionV1();
   }
 
-  public static class LongestPalindromicSubstringSolution implements
+  public static class LongestPalindromicSubstringSolutionV1 implements
       LongestPalindromicSubstringInterface {
 
     @Override
     public String longestPalindrome(String s) {
-      return null;
+      if (s == null || s.length() == 1) {
+        return s;
+      }
+      if (s.length() == 2) {
+        if (s.charAt(0) == s.charAt(1)) {
+          return s;
+        } else {
+          return String.valueOf(s.charAt(0));
+        }
+      }
+
+      int length = s.length();
+      for (int i = length; i > 0; i--) {
+        int start = 0;
+        int end = i;
+        while (end < length) {
+          int innerStart = start;
+          int innerEnd = end + 1;
+          StringBuffer sb = new StringBuffer();
+          while (innerStart < innerEnd) {
+            sb.append(s.charAt(innerStart++));
+          }
+          String str = sb.toString();
+          if (isPalindromStr(str)) {
+            return str;
+          }
+          start++;
+          end++;
+        }
+      }
+
+      return "";
+    }
+
+    private boolean isPalindromStr(String str) {
+      int start = 0;
+      int end = str.length() - 1;
+      while (start < end) {
+        if (str.charAt(start++) != str.charAt(end--)) {
+          return false;
+        }
+      }
+      return true;
     }
   }
 
