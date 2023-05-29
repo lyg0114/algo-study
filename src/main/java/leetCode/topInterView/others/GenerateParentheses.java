ae1e1720264a409edcb2fb9b8fcdbf26becc53c6
lyg0114 <whdnseowkd@naver.com>
2023-05-29 15:45:10 +0900

    Add : add GenerateParenthesesSolutionV2

--- src/main/java/leetCode/topInterView/others/GenerateParentheses.java
+++ src/main/java/leetCode/topInterView/others/GenerateParentheses.java
@@ -24,7 +24,31 @@ public class GenerateParentheses {
   }
 
   public static GenerateParenthesesInterface getSolution() {
-    return new GenerateParenthesesSolutionV1();
+    return new GenerateParenthesesSolutionV2();
+  }
+
+  public static class GenerateParenthesesSolutionV2 implements GenerateParenthesesInterface {
+
+    @Override
+    public List<String> generateParenthesis(int n) {
+      List<String> result = new ArrayList<>();
+      backtrack(result, "", 0, 0, n);
+      return result;
+    }
+
+    private void backtrack(List<String> result, String current, int open, int close, int max) {
+      if (current.length() == max * 2) {
+        result.add(current);
+        return;
+      }
+
+      if (open < max) {
+        backtrack(result, current + "(", open + 1, close, max);
+      }
+      if (close < open) {
+        backtrack(result, current + ")", open, close + 1, max);
+      }
+    }
   }
 
   public static class GenerateParenthesesSolutionV1 implements GenerateParenthesesInterface {
