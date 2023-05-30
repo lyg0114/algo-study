09baf53c2b88c878a2de7da5273e68c7fa9ffc4b
lyg0114 <whdnseowkd@naver.com>
2023-05-30 15:08:10 +0900

    Add : add SearchRotatedSortedArraySolutionV1

--- src/test/java/leetCode/topInterView/Array/SearchRotatedSortedArrayTest.java
+++ src/test/java/leetCode/topInterView/Array/SearchRotatedSortedArrayTest.java
@@ -13,10 +13,50 @@ import org.junit.Test;
 public class SearchRotatedSortedArrayTest {
 
   @Test
-  public void test_search() {
+  public void test_search_1() {
     SearchRotatedSortedArrayInterface solution = SearchRotatedSortedArray.getSolution();
     int[] nums = {4, 5, 6, 7, 0, 1, 2};
     int actual = solution.search(nums, 0);
     assertEquals(4, actual);
+  }
+
+  @Test
+  public void test_search_2() {
+    SearchRotatedSortedArrayInterface solution = SearchRotatedSortedArray.getSolution();
+    int[] nums = {4, 5, 6, 7, 0, 1, 2};
+    int actual = solution.search(nums, 3);
+    assertEquals(-1, actual);
+  }
+
+  @Test
+  public void test_search_3() {
+    SearchRotatedSortedArrayInterface solution = SearchRotatedSortedArray.getSolution();
+    int[] nums = {1};
+    int actual = solution.search(nums, 0);
+    assertEquals(-1, actual);
+  }
+
+  @Test
+  public void test_search_4() {
+    SearchRotatedSortedArrayInterface solution = SearchRotatedSortedArray.getSolution();
+    int[] nums = {1, 3};
+    int actual = solution.search(nums, 0);
+    assertEquals(-1, actual);
+  }
+
+  @Test
+  public void test_search_5() {
+    SearchRotatedSortedArrayInterface solution = SearchRotatedSortedArray.getSolution();
+    int[] nums = {1, 3};
+    int actual = solution.search(nums, 1);
+    assertEquals(0, actual);
+  }
+
+  @Test
+  public void test_search_6() {
+    SearchRotatedSortedArrayInterface solution = SearchRotatedSortedArray.getSolution();
+    int[] nums = {1, 3};
+    int actual = solution.search(nums, 3);
+    assertEquals(1, actual);
   }
 }
