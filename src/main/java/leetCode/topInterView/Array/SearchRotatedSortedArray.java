57fe8ed0333dff833e6e3c6b04bd3ed43fe17fd2
lyg0114 <whdnseowkd@naver.com>
2023-05-31 22:28:40 +0900

    Update : refactoring if condition

--- src/main/java/leetCode/topInterView/Array/SearchRotatedSortedArray.java
+++ src/main/java/leetCode/topInterView/Array/SearchRotatedSortedArray.java
@@ -36,13 +36,13 @@ public class SearchRotatedSortedArray {
         }
 
         if (nums[left] <= nums[mid]) {
-          if (target >= nums[left] && target < nums[mid]) {
+          if (nums[left] <= target && target < nums[mid]) {
             right = mid - 1;
           } else {
             left = mid + 1;
           }
         } else { // nums[left] > nums[mid]
-          if (target > nums[mid] && target <= nums[right]) {
+          if (nums[mid] < target && target <= nums[right]) {
             left = mid + 1;
           } else {
             right = mid - 1;
