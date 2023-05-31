cc1f71b68ca6358f68b3babe8f317e06493a368a
lyg0114 <whdnseowkd@naver.com>
2023-05-31 17:35:09 +0900

    Update : update calculate mid value

--- src/main/java/leetCode/topInterView/Array/SearchRotatedSortedArray.java
+++ src/main/java/leetCode/topInterView/Array/SearchRotatedSortedArray.java
@@ -29,7 +29,7 @@ public class SearchRotatedSortedArray {
       int right = nums.length - 1;
 
       while (left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = (left + right) / 2;
 
         if (nums[mid] == target) {
           return mid;
