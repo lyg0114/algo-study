82ace198b2e2f7b1d93b7f139f317d33f7cc9803
lyg0114 <whdnseowkd@naver.com>
2023-06-01 14:55:17 +0900

    Add : add fail test code

---
+++ src/test/java/leetCode/topInterView/Array/FindFirstLastPositionElementSortedArrayTest.java
@@ -0,0 +1,27 @@
+package leetCode.topInterView.Array;
+
+import static org.junit.Assert.*;
+
+import leetCode.topInterView.Array.FindFirstLastPositionElementSortedArray.FindFirstLastPositionElementSortedArrayInterface;
+import org.junit.Test;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetCode.topInterView.Array
+ * @since : 2023/06/01
+ */
+public class FindFirstLastPositionElementSortedArrayTest {
+
+  @Test
+  public void test_searchRange() {
+    FindFirstLastPositionElementSortedArrayInterface solution = FindFirstLastPositionElementSortedArray
+        .getSolution();
+
+    int[] arr = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+    int[] actuals = solution.searchRange(arr, target);
+    int[] expects = {3,4};
+    assertEquals(expects, actuals);
+  }
+
+}
