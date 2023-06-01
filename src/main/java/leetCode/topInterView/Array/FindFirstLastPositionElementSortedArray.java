731f92c616ed2430ad9c271cc81bad79cfdf1881
lyg0114 <whdnseowkd@naver.com>
2023-06-01 14:55:03 +0900

    Add : add interface and impl

--- src/main/java/leetCode/topInterView/Array/FindFirstLastPositionElementSortedArray.java
+++ src/main/java/leetCode/topInterView/Array/FindFirstLastPositionElementSortedArray.java
@@ -7,4 +7,29 @@ package leetCode.topInterView.Array;
  */
 public class FindFirstLastPositionElementSortedArray {
 
+  public static void main(String[] args) {
+    FindFirstLastPositionElementSortedArrayInterface solution = getSolution();
+    int[] arr = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+    int[] actuals = solution.searchRange(arr, target);
+    System.out.println("actuals = " + actuals);
+  }
+
+  public static FindFirstLastPositionElementSortedArrayInterface getSolution() {
+    return new FindFirstLastPositionElementSortedArraySolutionV1();
+  }
+
+  public static class FindFirstLastPositionElementSortedArraySolutionV1 implements
+      FindFirstLastPositionElementSortedArrayInterface {
+
+    @Override
+    public int[] searchRange(int[] nums, int target) {
+      return null;
+    }
+  }
+
+  public interface FindFirstLastPositionElementSortedArrayInterface {
+
+    int[] searchRange(int[] nums, int target);
+  }
 }
