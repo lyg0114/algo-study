3653c1b176da854a3856bbc9f038cbac63776108
lyg0114 <whdnseowkd@naver.com>
2023-06-02 16:57:46 +0900

    Update : add ReverseStr and refactoring

--- src/main/java/leetCode/topInterView/Strings/ReverseStr.java
+++ src/main/java/leetCode/topInterView/Strings/ReverseStr.java
@@ -8,23 +8,38 @@ package leetCode.topInterView.Strings;
 public class ReverseStr {
 
   public static void main(String[] args) {
-    String str = "HelloWorld";
+    String str = "Hello World";
     System.out.println("str = " + str);
-    char[] chars = str.toCharArray();
-    int l = 0;
-    int r = chars.length - 1;
-    char tmp;
-    while (l <= r) {
-      tmp = chars[r];
-      chars[r] = chars[l];
-      chars[l] = tmp;
-      l++;
-      r--;
-    }
+    ReverseStrInterface solution = getSolution();
+    String s = solution.reverseArr(str);
+    System.out.println("s = " + s);
+  }
 
-    for (int i = 0; i < chars.length; i++) {
-      System.out.println("chars[i] = " + chars[i]);
-    }
+  public static ReverseStrInterface getSolution() {
+    return new ReverseStrSolutionV1();
+  }
 
+  public static class ReverseStrSolutionV1 implements ReverseStrInterface {
+
+    @Override
+    public String reverseArr(String input) {
+      char[] chars = input.toCharArray();
+      int l = 0;
+      int r = chars.length - 1;
+      char tmp;
+      while (l <= r) {
+        tmp = chars[r];
+        chars[r] = chars[l];
+        chars[l] = tmp;
+        l++;
+        r--;
+      }
+      return new String(chars);
+    }
+  }
+
+  public interface ReverseStrInterface {
+
+    String reverseArr(String input);
   }
 }
