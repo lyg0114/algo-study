d47e1d4e4959c861385ca84ce330b57344325d5d
lyg0114 <whdnseowkd@naver.com>
2023-06-03 09:38:47 +0900

    Add : add test case

--- src/test/java/leetCode/topInterView/Array/MedianTwoSortedArraysTest.java
+++ src/test/java/leetCode/topInterView/Array/MedianTwoSortedArraysTest.java
@@ -15,10 +15,22 @@ public class MedianTwoSortedArraysTest {
   @Test
   public void test_findMedianSortedArrays_1() {
     MedianTwoSortedArraysInterface solution = MedianTwoSortedArrays.getSolution();
-    int[] nums1 = new int[10];
-    int[] nums2 = new int[10];
-    double expect = 2.00000;
+    int[] nums1 = {1, 2};
+    int[] nums2 = {3, 4};
+    double expected = 2.50000;
+    double delta = 0.00001; // 허용 오차 범위
     double actual = solution.findMedianSortedArrays(nums1, nums2);
-    assertEquals(expect, actual);
+    assertEquals(expected, actual, delta);
+  }
+
+  @Test
+  public void test_findMedianSortedArrays_2() {
+    MedianTwoSortedArraysInterface solution = MedianTwoSortedArrays.getSolution();
+    int[] nums1 = {1};
+    int[] nums2 = {3, 4};
+    double expected = 3.0;
+    double delta = 0.00001; // 허용 오차 범위
+    double actual = solution.findMedianSortedArrays(nums1, nums2);
+    assertEquals(expected, actual, delta);
   }
 }
