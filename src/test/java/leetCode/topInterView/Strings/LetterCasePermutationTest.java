9f50191f444122f15a2ff1c303d59c13294c479e
lyg0114 <whdnseowkd@naver.com>
2023-06-04 16:02:46 +0900

    Add : add test case

--- src/test/java/leetCode/topInterView/Strings/LetterCasePermutationTest.java
+++ src/test/java/leetCode/topInterView/Strings/LetterCasePermutationTest.java
@@ -22,4 +22,31 @@ public class LetterCasePermutationTest {
     List<String> expects = Arrays.asList("a1b2", "a1B2", "A1b2", "A1B2");
     assertEquals(expects, actuals);
   }
+
+  @Test
+  public void test_letterCasePermutation_2() {
+    LetterCasePermutationInteface solution = LetterCasePermutation.getSolution();
+    String in = "C";
+    List<String> actuals = solution.letterCasePermutation(in);
+    List<String> expects = Arrays.asList("C", "c");
+    assertEquals(expects, actuals);
+  }
+
+  @Test
+  public void test_letterCasePermutation_3() {
+    LetterCasePermutationInteface solution = LetterCasePermutation.getSolution();
+    String in = "c";
+    List<String> actuals = solution.letterCasePermutation(in);
+    List<String> expects = Arrays.asList("c", "C");
+    assertEquals(expects, actuals);
+  }
+
+  @Test
+  public void test_letterCasePermutation_4() {
+    LetterCasePermutationInteface solution = LetterCasePermutation.getSolution();
+    String in = "po";
+    List<String> actuals = solution.letterCasePermutation(in);
+    List<String> expects = Arrays.asList("po", "pO", "Po", "PO");
+    assertEquals(expects, actuals);
+  }
 }
