dfac2ba14a68aa0374531047c43a385c489c6914
lyg0114 <whdnseowkd@naver.com>
2023-06-08 10:39:05 +0900

    Impl : HappyNumberSolutionV1

--- src/main/java/leetCode/topInterView/others/HappyNumber.java
+++ src/main/java/leetCode/topInterView/others/HappyNumber.java
@@ -1,5 +1,8 @@
 package leetCode.topInterView.others;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.others
@@ -19,7 +22,28 @@ public class HappyNumber {
 
     @Override
     public boolean isHappy(int n) {
-      return false;
+      if (!(Integer.MIN_VALUE < n && n < Integer.MAX_VALUE)) {
+        return false;
+      }
+
+      String s = String.valueOf(n);
+      char[] nums = s.toCharArray();
+      int res = 0;
+      List<String> valid = new ArrayList<String>();
+      while (res != 1) {
+        res = 0;
+        for (char num : nums) {
+          int cur = Integer.parseInt(String.valueOf(num));
+          res += cur * cur;
+        }
+        s = String.valueOf(res);
+        if (valid.contains(s)) {
+          return false;
+        }
+        valid.add(s);
+        nums = s.toCharArray();
+      }
+      return true;
     }
   }
 
