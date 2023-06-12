e0fe2300bff91f1d1961b38cd2d1bc1b21b52d6c
lyg0114 <whdnseowkd@naver.com>
2023-06-12 22:21:20 +0900

    Update : refactoring

--- src/test/java/leetCode/topInterView/others/ExcelSheetColumnNumber.java
+++ src/test/java/leetCode/topInterView/others/ExcelSheetColumnNumber.java
@@ -6,10 +6,11 @@ import java.util.Map;
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.others
- * @since : 2023/06/11
  * @url : https://leetcode.com/problems/excel-sheet-column-number/description/?envType=featured-list&envId=top-interview-questions
+ * @since : 2023/06/11
  */
 public class ExcelSheetColumnNumber {
+
   public static void main(String[] args) {
     ExcelSheetColumnNumberInterface solution = getSolution();
     int res = solution.titleToNumber("ZY");
@@ -33,12 +34,33 @@ public class ExcelSheetColumnNumber {
   // ...
   // ABC -> (26 x 26) x 1 + (26) x 2 + 3 =
   public static class ExcelSheetColumnNumberSolutionV1 implements ExcelSheetColumnNumberInterface {
+
     private final Map<Character, Integer> mp = new HashMap<>() {{
-      put('A', 1); put('B', 2); put('C', 3); put('D', 4); put('E', 5);
-      put('F', 6); put('G', 7); put('H', 8); put('I', 9); put('J', 10);
-      put('K', 11); put('L', 12); put('M', 13); put('N', 14); put('O', 15);
-      put('P', 16); put('Q', 17); put('R', 18); put('S', 19); put('T', 20);
-      put('U', 21); put('V', 22); put('W', 23); put('X', 24); put('Y', 25);
+      put('A', 1);
+      put('B', 2);
+      put('C', 3);
+      put('D', 4);
+      put('E', 5);
+      put('F', 6);
+      put('G', 7);
+      put('H', 8);
+      put('I', 9);
+      put('J', 10);
+      put('K', 11);
+      put('L', 12);
+      put('M', 13);
+      put('N', 14);
+      put('O', 15);
+      put('P', 16);
+      put('Q', 17);
+      put('R', 18);
+      put('S', 19);
+      put('T', 20);
+      put('U', 21);
+      put('V', 22);
+      put('W', 23);
+      put('X', 24);
+      put('Y', 25);
       put('Z', 26);
     }};
 
@@ -70,6 +92,7 @@ public class ExcelSheetColumnNumber {
   }
 
   public interface ExcelSheetColumnNumberInterface {
+
     int titleToNumber(String columnTitle);
   }
 }
