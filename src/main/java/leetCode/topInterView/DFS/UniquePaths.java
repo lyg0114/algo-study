c2e21967cd76604abc33fce597278e9678e75eb3
lyg0114 <whdnseowkd@naver.com>
2023-06-13 16:29:26 +0900

    Add : UniquePathsSolutionV1

--- src/main/java/leetCode/topInterView/DFS/UniquePaths.java
+++ src/main/java/leetCode/topInterView/DFS/UniquePaths.java
@@ -9,7 +9,7 @@ public class UniquePaths {
 
   public static void main(String[] args) {
     UniquePathsInterface solution = getSolution();
-    int res = solution.uniquePaths(3, 7);
+    int res = solution.uniquePaths(3, 2);
     System.out.println("res = " + res);
   }
 
@@ -19,9 +19,30 @@ public class UniquePaths {
 
   public static class UniquePathsSolutionV1 implements UniquePathsInterface {
 
+    int[] dx = {1, 0};
+    int[] dy = {0, 1};
+    int res = 0;
+
     @Override
     public int uniquePaths(int m, int n) {
-      return 0;
+      int[][] mx = new int[m][n];
+      findWay(mx, 0, 0, m, n);
+      return res;
+    }
+
+    public void findWay(int[][] mx, int cx, int cy, int m, int n) {
+      if (cx == (n - 1) && cy == (m - 1)) {
+        res++;
+        return;
+      }
+
+      if (!((0 <= cx && cx < n) && (0 <= cy && cy < m))) {
+        return;
+      }
+
+      for (int i = 0; i < 2; i++) {
+        findWay(mx, cx + dx[i], cy + dy[i], m, n);
+      }
     }
   }
 
