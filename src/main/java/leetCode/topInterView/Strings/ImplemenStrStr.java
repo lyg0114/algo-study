c3d4ff2f4252521f2a84bc495a2757d844cbd3a0
lyg0114 <whdnseowkd@naver.com>
2023-06-18 17:22:44 +0900

    Update : make static class ImplemenStrStrSolution

--- src/main/java/leetCode/topInterView/Strings/ImplemenStrStr.java
+++ src/main/java/leetCode/topInterView/Strings/ImplemenStrStr.java
@@ -13,7 +13,6 @@ package leetCode.topInterView.Strings;
    3.2 가져온 needle의 문자와 haystack의 저장된 인덱스 + 1 의 문자를 확인한다.
    4.1 없으면 haystack의 마지막 인덱스부터 다시 시작
 */
-
 public class ImplemenStrStr {
 
   public static void main(String[] args) {
@@ -22,26 +21,32 @@ public class ImplemenStrStr {
     ImplemenStrStrSolution solution = new ImplemenStrStrSolution();
     System.out.println(solution.strStr(haystack, needle));
   }
-}
 
-class ImplemenStrStrSolution {
+  public static class ImplemenStrStrSolution {
 
-  public int strStr(String haystack, String needle) {
-    // needle의 길이가 0인 경우 항상 0을 반환합니다.
-    if (needle.length() == 0) {
-      return 0;
-    }
-    // haystack의 길이가 needle의 길이보다 작은 경우 -1을 반환합니다.
-    if (haystack.length() < needle.length()) {
+    public int strStr(String haystack, String needle) {
+      // needle의 길이가 0인 경우 항상 0을 반환합니다.
+      if (needle.length() == 0) {
+        return 0;
+      }
+      // haystack의 길이가 needle의 길이보다 작은 경우 -1을 반환합니다.
+      if (haystack.length() < needle.length()) {
+        return -1;
+      }
+      // haystack 문자열을 탐색하여 needle 문자열이 처음 나타나는 위치를 찾습니다.
+      for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+        if (haystack.substring(i, i + needle.length()).equals(needle)) {
+          return i;
+        }
+      }
+      // needle 문자열이 haystack 문자열에 없는 경우 -1을 반환합니다.
       return -1;
     }
-    // haystack 문자열을 탐색하여 needle 문자열이 처음 나타나는 위치를 찾습니다.
-    for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-      if (haystack.substring(i, i + needle.length()).equals(needle)) {
-        return i;
-      }
-    }
-    // needle 문자열이 haystack 문자열에 없는 경우 -1을 반환합니다.
-    return -1;
+  }
+
+  public interface ImplemenStrStrInterface {
+
+    int strStr(String haystack, String needle);
   }
 }
+
