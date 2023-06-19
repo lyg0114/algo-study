d6af8845f3368fa699f4a56b4fd4bba497f7e202
lyg0114 <whdnseowkd@naver.com>
2023-06-19 12:44:23 +0900

    Update : refactoring ReverseInteger

--- src/main/java/leetCode/topInterView/Strings/ReverseInteger.java
+++ src/main/java/leetCode/topInterView/Strings/ReverseInteger.java
@@ -3,30 +3,40 @@ package leetCode.topInterView.Strings;
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.Strings
- * @since : 2023/03/08
- * https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/
+ * @since : 2023/03/08 https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/
  */
 public class ReverseInteger {
 
   public static void main(String[] args) {
-    ReverseIntegerSolution solution = new ReverseIntegerSolution();
+    ReverseIntegerInterface solution = getSolution();
     int num = 123;
-    solution.reverse(num);
+    int reverse = solution.reverse(num);
+    System.out.println("reverse = " + reverse);
   }
-}
 
-class ReverseIntegerSolution {
+  public static ReverseIntegerInterface getSolution() {
+    return new ReverseIntegerSolution();
+  }
 
-  public int reverse(int x) {
-    int rev = 0;
-    while (x != 0) {
-      int m = x % 10;
-      x = x / 10;
-      if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
-        return 0;
+  public static class ReverseIntegerSolution implements ReverseIntegerInterface {
+
+    public int reverse(int x) {
+      int rev = 0;
+      while (x != 0) {
+        int m = x % 10;
+        x = x / 10;
+        if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+          return 0;
+        }
+        rev = (rev * 10) + m;
       }
-      rev = (rev * 10) + m;
+      return rev;
     }
-    return rev;
+  }
+
+  public interface ReverseIntegerInterface {
+
+    int reverse(int x);
   }
 }
+
