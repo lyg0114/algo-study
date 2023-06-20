9f67e107eb69da34b484e297db7eeb5ac7b6e866
lyg0114 <whdnseowkd@naver.com>
2023-06-20 10:16:45 +0900

    Update : refactoring ValidAnagram

--- src/main/java/leetCode/topInterView/Strings/ValidAnagram.java
+++ src/main/java/leetCode/topInterView/Strings/ValidAnagram.java
@@ -10,41 +10,45 @@ public class ValidAnagram {
   public static void main(String[] args) {
     String s = "anagram";
     String t = "nagaram";
-    ValidAnagramSolution solution = new ValidAnagramSolution();
+    ValidAnagramInterface solution = getSolution();
     System.out.println(solution.isAnagram(s, t));
   }
-}
 
-class ValidAnagramSolution {
+  public static ValidAnagramInterface getSolution() {
+    return new ValidAnagramSolutionV1();
+  }
 
-  public boolean isAnagram(String s, String t) {
-    int[] countArrs1 = getCountArrs(s);
-    int[] countArrs2 = getCountArrs(t);
-    int totalCnt = countArrs1.length == countArrs2.length ? countArrs1.length : -1;
+  static class ValidAnagramSolutionV1 implements ValidAnagramInterface {
 
-    if (totalCnt == -1) {
-      return false;
-    }
-    for (int i = 0; i < totalCnt; i++) {
-      if (countArrs1[i] != countArrs2[i]) {
+    public boolean isAnagram(String s, String t) {
+      int[] countArrs1 = getCountArrs(s);
+      int[] countArrs2 = getCountArrs(t);
+      int totalCnt = countArrs1.length == countArrs2.length ? countArrs1.length : -1;
+
+      if (totalCnt == -1) {
         return false;
       }
+      for (int i = 0; i < totalCnt; i++) {
+        if (countArrs1[i] != countArrs2[i]) {
+          return false;
+        }
+      }
+      return true;
     }
-    return true;
+
+    private int[] getCountArrs(String str) {
+      int[] cntArr = new int[26];
+      for (int i = 0; i < str.length(); i++) {
+        cntArr[str.charAt(i) - 'a']++;
+      }
+      return cntArr;
+    }
   }
 
-  private int[] getCountArrs(String str) {
-    int[] cntArr = new int[26];
-    for (int i = 0; i < str.length(); i++) {
-      cntArr[str.charAt(i) - 'a']++;
-    }
-    return cntArr;
+  public interface ValidAnagramInterface {
+
+    boolean isAnagram(String s, String t);
   }
 }
-
-
-
-
-
 
 
