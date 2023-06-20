2bcee00bb0fbbd27ec5a60756def51b0e59beaa3
lyg0114 <whdnseowkd@naver.com>
2023-06-20 13:54:23 +0900

    Add : PalindromePartitioningSolutionV1

--- src/main/java/leetCode/topInterView/Strings/PalindromePartitioning.java
+++ src/main/java/leetCode/topInterView/Strings/PalindromePartitioning.java
@@ -1,5 +1,6 @@
 package leetCode.topInterView.Strings;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,7 +28,38 @@ public class PalindromePartitioning {
 
     @Override
     public List<List<String>> partition(String s) {
-      return null;
+      List<List<String>> result = new ArrayList<>();
+      backtrack(s, 0, new ArrayList<>(), result);
+      return result;
+    }
+
+    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+      if (start == s.length()) {
+        result.add(new ArrayList<>(path));
+        return;
+      }
+
+      for (int end = start + 1; end <= s.length(); end++) {
+        String sub = s.substring(start, end);
+        if (isPalindrome(sub)) {
+          path.add(sub);
+          backtrack(s, end, path, result);
+          path.remove(path.size() - 1);
+        }
+      }
+    }
+
+    private boolean isPalindrome(String s) {
+      int left = 0;
+      int right = s.length() - 1;
+      while (left < right) {
+        if (s.charAt(left) != s.charAt(right)) {
+          return false;
+        }
+        left++;
+        right--;
+      }
+      return true;
     }
   }
 
