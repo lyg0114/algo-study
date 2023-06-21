1e520106afeb02e1ead000f1817441fd9263be72
lyg0114 <whdnseowkd@naver.com>
2023-06-21 15:38:26 +0900

    Update : refactoring ContainsDuplicate

--- src/main/java/leetCode/topInterView/Array/ContainsDuplicate.java
+++ src/main/java/leetCode/topInterView/Array/ContainsDuplicate.java
@@ -10,22 +10,33 @@ import java.util.HashSet;
 public class ContainsDuplicate {
 
   public static void main(String[] args) {
-    ContainsDuplicateSolution solution = new ContainsDuplicateSolution();
+    ContainsDuplicateInterface solution = getSolution();
     int[] nums = {1, 2, 3, 1};
     System.out.println(solution.containsDuplicate(nums));
   }
-}
 
-class ContainsDuplicateSolution {
+  private static ContainsDuplicateInterface getSolution() {
+    return new ContainsDuplicateSolutionV1();
+  }
 
-  public boolean containsDuplicate(int[] nums) {
-    HashSet<Integer> set = new HashSet<Integer>();
-    for (int i = 0; i < nums.length; i++) {
-      if (set.contains(nums[i])) {
-        return true;
+  public static class ContainsDuplicateSolutionV1 implements ContainsDuplicateInterface {
+
+    @Override
+    public boolean containsDuplicate(int[] nums) {
+      HashSet<Integer> set = new HashSet<Integer>();
+      for (int i = 0; i < nums.length; i++) {
+        if (set.contains(nums[i])) {
+          return true;
+        }
+        set.add(nums[i]);
       }
-      set.add(nums[i]);
+      return false;
     }
-    return false;
+  }
+
+  public interface ContainsDuplicateInterface {
+
+    boolean containsDuplicate(int[] nums);
   }
 }
+
