39ef6f16fe4df4ddcda888e78ce5e82404c4b70f
lyg0114 <whdnseowkd@naver.com>
2023-06-23 16:10:25 +0900

    Add : KthLargestElementArraySolutionV1

--- src/main/java/leetCode/topInterView/Array/KthLargestElementArray.java
+++ src/main/java/leetCode/topInterView/Array/KthLargestElementArray.java
@@ -1,5 +1,7 @@
 package leetCode.topInterView.Array;
 
+import java.util.Random;
+
 /**
  * @author : iyeong-gyo
  * @package : leetCode.topInterView.Array
@@ -10,7 +12,8 @@ public class KthLargestElementArray {
   public static void main(String[] args) {
     KthLargestElementArrayInterface solution = getSolution();
     int[] nums = {3, 2, 1, 5, 6, 4};
-    solution.findKthLargest(nums, 2);
+    int kthLargest = solution.findKthLargest(nums, 2);
+    System.out.println("kthLargest = " + kthLargest);
   }
 
   private static KthLargestElementArrayInterface getSolution() {
@@ -19,9 +22,51 @@ public class KthLargestElementArray {
 
   public static class KthLargestElementArraySolutionV1 implements KthLargestElementArrayInterface {
 
-    @Override
+    private static Random random = new Random();
+
     public int findKthLargest(int[] nums, int k) {
-      return 0;
+      int left = 0;
+      int right = nums.length - 1;
+
+      int targetIndex = nums.length - k;
+
+      while (left <= right) {
+        int pivotIndex = partition(nums, left, right);
+
+        if (pivotIndex == targetIndex) {
+          return nums[pivotIndex];
+        } else if (pivotIndex < targetIndex) {
+          left = pivotIndex + 1;
+        } else {
+          right = pivotIndex - 1;
+        }
+      }
+
+      return -1;
+    }
+
+    private int partition(int[] nums, int left, int right) {
+      int randomIndex = random.nextInt(right - left + 1) + left;
+      swap(nums, randomIndex, right);
+
+      int pivot = nums[right];
+      int i = left - 1;
+
+      for (int j = left; j < right; j++) {
+        if (nums[j] <= pivot) {
+          i++;
+          swap(nums, i, j);
+        }
+      }
+
+      swap(nums, i + 1, right);
+      return i + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+      int temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
     }
   }
 
