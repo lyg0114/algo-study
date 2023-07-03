94851affe2c8c7b40c0a15b57d7e146a331db483
lyg0114 <whdnseowkd@naver.com>
2023-07-03 19:11:52 +0900

    Add : KthLargestElementArraySolutionV2

--- src/main/java/leetCode/topInterView/Array/KthLargestElementArray.java
+++ src/main/java/leetCode/topInterView/Array/KthLargestElementArray.java
@@ -16,8 +16,48 @@ public class KthLargestElementArray {
     System.out.println("kthLargest = " + kthLargest);
   }
 
-  private static KthLargestElementArrayInterface getSolution() {
+  public static KthLargestElementArrayInterface getSolution() {
     return new KthLargestElementArraySolutionV1();
+  }
+
+  public static class KthLargestElementArraySolutionV2 implements KthLargestElementArrayInterface {
+
+    public int findKthLargest(int[] nums, int k) {
+      return quickSelect(nums, 0, nums.length - 1, k);
+    }
+
+    int quickSelect(int[] nums, int low, int high, int k) {
+      int pivot = low;
+
+      // use quick sort's idea
+      // put nums that are <= pivot to the left
+      // put nums that are  > pivot to the right
+      for (int j = low; j < high; j++) {
+        if (nums[j] <= nums[high]) {
+          swap(nums, pivot++, j);
+        }
+      }
+      swap(nums, pivot, high);
+
+      // count the nums that are > pivot from high
+      int count = high - pivot + 1;
+      // pivot is the one!
+      if (count == k) {
+        return nums[pivot];
+      }
+      // pivot is too small, so it must be on the right
+      if (count > k) {
+        return quickSelect(nums, pivot + 1, high, k);
+      }
+      // pivot is too big, so it must be on the left
+      return quickSelect(nums, low, pivot - 1, k - count);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+      int temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+    }
   }
 
   public static class KthLargestElementArraySolutionV1 implements KthLargestElementArrayInterface {
