34ad815e399680a155d54ded487051fff061424f
lyg0114 <whdnseowkd@naver.com>
2023-07-03 19:13:31 +0900

    Update : refactoring KthLargestElementArraySolutionV2

--- src/main/java/leetCode/topInterView/Array/KthLargestElementArray.java
+++ src/main/java/leetCode/topInterView/Array/KthLargestElementArray.java
@@ -29,9 +29,6 @@ public class KthLargestElementArray {
     int quickSelect(int[] nums, int low, int high, int k) {
       int pivot = low;
 
-      // use quick sort's idea
-      // put nums that are <= pivot to the left
-      // put nums that are  > pivot to the right
       for (int j = low; j < high; j++) {
         if (nums[j] <= nums[high]) {
           swap(nums, pivot++, j);
@@ -39,17 +36,13 @@ public class KthLargestElementArray {
       }
       swap(nums, pivot, high);
 
-      // count the nums that are > pivot from high
       int count = high - pivot + 1;
-      // pivot is the one!
       if (count == k) {
         return nums[pivot];
       }
-      // pivot is too small, so it must be on the right
       if (count > k) {
         return quickSelect(nums, pivot + 1, high, k);
       }
-      // pivot is too big, so it must be on the left
       return quickSelect(nums, low, pivot - 1, k - count);
     }
 
