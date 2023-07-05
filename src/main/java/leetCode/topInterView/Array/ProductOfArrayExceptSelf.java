49505692c391c7adf1324704e00162831b8c88c9
lyg0114 <whdnseowkd@naver.com>
2023-07-05 23:16:44 +0900

    Add : ProductOfArrayExceptSelfSolutionV1

--- src/main/java/leetCode/topInterView/Array/ProductOfArrayExceptSelf.java
+++ src/main/java/leetCode/topInterView/Array/ProductOfArrayExceptSelf.java
@@ -25,7 +25,21 @@ public class ProductOfArrayExceptSelf {
 
     @Override
     public int[] productExceptSelf(int[] nums) {
-      return new int[0];
+      int n = nums.length;
+      int[] answer = new int[n];
+      answer[0] = 1;
+
+      for (int i = 1; i < n; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+      }
+
+      int rightProduct = 1;
+      for (int i = n - 1; i >= 0; i--) {
+        answer[i] *= rightProduct;
+        rightProduct *= nums[i];
+      }
+
+      return answer;
     }
   }
 
