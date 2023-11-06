ba3912e954d18585c9e7b94045c154b1fac0f900
lyg0114 <whdnseowkd@naver.com>
2023-11-06 17:52:37 +0100

    Complete : SqrtX

--- src/main/java/leetCode/topInterView/Math/SqrtX.java
+++ src/main/java/leetCode/topInterView/Math/SqrtX.java
@@ -9,7 +9,7 @@ public class SqrtX {
 
   public static void main(String[] args) {
     SqrtXInterface solution = getSolution();
-    int input = 3;
+    int input = 123;
     int result = solution.mySqrt(input);
     System.out.println("result = " + result);
   }
@@ -22,7 +22,25 @@ public class SqrtX {
 
     @Override
     public int mySqrt(int x) {
-      return 0;
+      if (x == 0 || x == 1) {
+        return x;
+      }
+
+      int start = 1;
+      int end = x;
+      int mid = -1;
+
+      while (start <= end) {
+        mid = start + (end - start) / 2;
+        if ((long) mid * mid > (long) x)
+          end = mid - 1;
+        else if (mid * mid == x)
+          return mid;
+        else
+          start = mid + 1;
+      }
+      System.out.println("end = " + end);
+      return Math.round(end);
     }
   }
 
