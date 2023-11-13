62f8751881016be96ddff766b1224546e022bb9e
lyg0114 <whdnseowkd@naver.com>
2023-11-13 15:47:27 +0100

    Codility

--- src/main/java/codility/v2/CodilitySolution3.java
+++ src/main/java/codility/v2/CodilitySolution3.java
@@ -12,12 +12,50 @@ import java.util.List;
 public class CodilitySolution3 {
 
   public static void main(String[] args) {
+    String[] E = {"039", "4", "14", "32", "", "34", "7"};
     Solution solution = new Solution();
+    int solution1 = solution.solution(E);
+    System.out.println("solution1 = " + solution1);
   }
 
   static class Solution {
 
-    public void solution() {
+    public int solution(String[] E) {
+      int days = 10;
+      int[][] solutions = new int[E.length][days];
+      for (int i = 0; i < E.length; i++) {
+        char[] charArray = E[i].toCharArray();
+        for (int j = 0; j < charArray.length; j++) {
+          solutions[i][charArray[j] - '0'] = 1;
+        }
+      }
+
+      ArrayList<Integer> sums = new ArrayList<>();
+
+      for (int i = 0; i < days; i++) {
+        int sum = 0;
+        for (int j = 0; j < solutions.length; j++) {
+          sum += solutions[j][i];
+        }
+        sums.add(sum);
+      }
+
+      for (Integer integer : sums) {
+        System.out.println("integer = " + integer);
+      }
+
+      sums.sort(Collections.reverseOrder());
+      int firstMax = sums.get(0);
+      int secondMax = sums.get(1);
+
+      int result = 0;
+      for (int i = 0; i < solutions.length; i++) {
+        if (solutions[i][firstMax] > 0 || solutions[i][secondMax] > 0) {
+          result++;
+        }
+      }
+
+      return result;
     }
   }
 }
