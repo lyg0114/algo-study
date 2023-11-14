3fd9170d7e1db2ef18afdfc35b1c1cc881ba97f3
lyg0114 <whdnseowkd@naver.com>
2023-11-14 13:51:01 +0100

    Add : JumpGameII45

---
+++ src/main/java/leetcode/topinterview/greedy/JumpGameII45.java
@@ -0,0 +1,21 @@
+package leetcode.topinterview.greedy;
+
+/**
+ * @author : iyeong-gyo
+ * @package : leetcode.topinterview.greedy
+ * @@url : https://leetcode.com/problems/jump-game-ii/description/
+ * @since : 14.11.23
+ */
+public class JumpGameII45 {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+  }
+
+  static class Solution {
+
+    public int jump(int[] nums) {
+      return 0;
+    }
+  }
+}
