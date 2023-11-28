dd17be4bc3ee4cbec19e07e21240f3e231f2c856
lyg0114 <whdnseowkd@naver.com>
2023-11-28 16:43:47 +0100

    feat : Add study test PriorityQueueTest

---
+++ src/test/java/study/PriorityQueueTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import org.junit.Test;
+
+/**
+ * @author : iyeong-gyo
+ * @package : study
+ * @since : 28.11.23
+ */
+public class PriorityQueueTest {
+
+  @Test
+  public void priorityQueuetest() {
+    PriorityQueue<Integer> minPriorityQueue = new PriorityQueue<>();
+    minPriorityQueue.add(3);
+    minPriorityQueue.add(1);
+    minPriorityQueue.add(4);
+    minPriorityQueue.add(2);
+    while (!minPriorityQueue.isEmpty()) {
+      System.out.println(minPriorityQueue.poll());
+    }
+  }
+
+  @Test
+  public void reversePriorityQueuetest() {
+    PriorityQueue<Integer> maxPriorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+    maxPriorityQueue.add(3);
+    maxPriorityQueue.add(1);
+    maxPriorityQueue.add(4);
+    maxPriorityQueue.add(2);
+    while (!maxPriorityQueue.isEmpty()) {
+      System.out.println(maxPriorityQueue.poll());
+    }
+  }
+}
