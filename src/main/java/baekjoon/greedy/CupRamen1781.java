b9862533acd6db5173ae4f539da32a039f65af94
lyg0114 <whdnseowkd@naver.com>
2023-12-23 13:01:45 +0100

    백준 : 컵라면

--- src/main/java/baekjoon/greedy/CupRamen1781.java
+++ src/main/java/baekjoon/greedy/CupRamen1781.java
@@ -1,8 +1,9 @@
 package baekjoon.greedy;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 /**
  * @author : iyeong-gyo
@@ -11,20 +12,48 @@ import java.io.InputStreamReader;
  */
 public class CupRamen1781 {
 
+  static class Req {
+    public final int ramyeon;
+    public final int deadLine;
+    public Req(int deadLine, int ramyeon) {
+      this.ramyeon = ramyeon;
+      this.deadLine = deadLine;
+    }
+  }
+
   public static void main(String[] args) {
+    Solution solution = new SolutionV1();
+    solution.doMain();
   }
 
   static class SolutionV1 implements Solution {
 
     @Override
-    public void doMain() throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      String str = br.readLine();
-      String boom = br.readLine();
+    public void doMain() {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      Req[] req = new Req[n];
+      for (int i = 0; i < n; i++) {
+        req[i] = new Req(sc.nextInt(), sc.nextInt());
+      }
+      Arrays.sort(req, Comparator.comparingInt(r -> r.deadLine));
+      PriorityQueue<Integer> pq = new PriorityQueue<>();
+      int result = 0;
+      for (Req r : req) {
+        pq.add(r.ramyeon);
+        if (pq.size() > r.deadLine) {
+          pq.poll();
+        }
+      }
+      while (!pq.isEmpty()) {
+        result += pq.poll();
+      }
+      System.out.println(result);
     }
   }
 
   interface Solution {
-    void doMain() throws IOException;
+
+    void doMain();
   }
 }
