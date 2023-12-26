9fd8bba02fc4f58702154d887ce9fc555388845e
lyg0114 <whdnseowkd@naver.com>
2023-12-26 03:38:22 +0100

    백준 : 소가 길을 건너간 이유 3

---
+++ src/main/java/baekjoon/greedy/Question14469.java
@@ -0,0 +1,77 @@
+package baekjoon.greedy;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.greedy
+ * @since : 23.12.23
+ */
+public class Question14469 {
+
+  public static void main(String[] args) throws IOException {
+    Solution solution = new SolutionV1();
+    solution.doMain();
+  }
+
+  static class SolutionV1 implements Solution {
+
+    static class ArriveGoat {
+      private int arrTime;
+      private final int checkTime;
+
+      public ArriveGoat(int arrTime, int checkTime) {
+        this.arrTime = arrTime;
+        this.checkTime = checkTime;
+      }
+      public int getArrTime() { return arrTime; }
+      public int getTerm(){return arrTime + checkTime;}
+      public void setArrTime(int arrTime) {
+        this.arrTime = arrTime;
+      }
+    }
+
+    @Override
+    public void doMain() throws IOException {
+      Scanner sc = new Scanner(System.in);
+      int totalCnt = sc.nextInt();
+      List<ArriveGoat> goats = new ArrayList<>();
+      for (int i = 0; i < totalCnt; i++) {
+        goats.add(new ArriveGoat(sc.nextInt(), sc.nextInt()));
+      }
+      goats.sort(Comparator.comparingInt(ArriveGoat::getArrTime));
+      Stack<ArriveGoat> stack = new Stack<>();
+      int totalTime = 0;
+      for (ArriveGoat arriveGoat : goats) {
+        if (stack.isEmpty()) {
+          totalTime = arriveGoat.getTerm();
+          stack.push(arriveGoat);
+        } else {
+          ArriveGoat peek = stack.peek();
+          if (peek.getTerm() <= arriveGoat.getArrTime()) {
+            stack.pop();
+            totalTime = arriveGoat.getTerm();
+            stack.push(arriveGoat);
+          } else {
+            arriveGoat.setArrTime(totalTime);
+            totalTime = arriveGoat.getTerm();
+            stack.pop();
+            stack.push(arriveGoat);
+          }
+        }
+      }
+      System.out.println("totalTime = " + totalTime);
+    }
+  }
+
+
+  interface Solution {
+
+    void doMain() throws IOException;
+  }
+}
