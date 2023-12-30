2f347f6ee5151a1e7eb38d4d1ba967a40521e319
lyg0114 <whdnseowkd@naver.com>
2023-12-30 10:53:45 +0100

    백준 : 미세먼지 안녕 - 17144 - V3  추가

--- src/main/java/baekjoon/greedy/Qustion17144.java
+++ src/main/java/baekjoon/greedy/Qustion17144.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 = example =
 ===========
 
- 7 7 1
+ 7 7 2
 2 3 4 6 7 8 9
 1 0 0 3 0 0 8
 -1 0 2 3 0 22 10
@@ -48,11 +48,141 @@ import java.util.Scanner;
  * @since : 29.12.23
  */
 public class Qustion17144 {
-
   public static void main(String[] args) throws IOException {
-    Solution solution = new SolutionImplV2();
+    Solution solution = new SolutionImplV3();
     solution.doMain();
   }
+
+  static class SolutionImplV3 implements Solution {
+    static class Point {
+      public int x;
+      public int y;
+      public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+      }
+    }
+
+    private int R = 0;
+    private int C = 0;
+    private int T = 0;
+    private int[][] mtx;
+    private int[][] tmp;
+    private List<Point> v1;
+    private List<Point> v2;
+
+    // 반시계
+    private int[] dx1 = {0, -1, 0, 1};
+    private int[] dy1 = {1, 0, -1, 0};
+    // 시계
+    private int[] dx2 = {0, 1, 0, -1};
+    private int[] dy2 = {1, 0, -1, 0};
+
+    @Override
+    public void doMain() throws IOException {
+      Scanner sc = new Scanner(System.in);
+      R = sc.nextInt();
+      C = sc.nextInt();
+      T = sc.nextInt();
+      mtx = new int[R][C];
+      boolean flag = true;
+      for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+          mtx[i][j] = sc.nextInt();
+          if (mtx[i][j] == -1) {
+            if (flag) {
+              v1 = chung(i, j, dx1, dy1);
+              flag = false;
+            } else {
+              v2 = chung(i, j, dx2, dy2);
+            }
+          }
+        }
+      }
+
+      for (int i = 0; i < T; i++) {
+        miseGo();
+        go(v1);
+        go(v2);
+      }
+
+      int ret = 0;
+      for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+          if (mtx[i][j] != -1) {
+            ret += mtx[i][j];
+          }
+        }
+      }
+      System.out.println(ret);
+    }
+
+    private void go(List<Point> v) {
+      for (int i = v.size() - 1; i > 0; i--) {
+        mtx[v.get(i).x][v.get(i).y] = mtx[v.get(i - 1).x][v.get(i - 1).y];
+      }
+      mtx[v.get(0).x][v.get(0).y] = 0;
+    }
+
+    private void miseGo() {
+      tmp = new int[R][C];
+      int spread, nx, ny;
+      for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+          if (mtx[i][j] > 0) {
+            spread = mtx[i][j] / 5;
+            int cnt = 0;
+            for (int k = 0; k < 4; k++) {
+              nx = i + dx1[k];
+              ny = j + dy1[k];
+              if (ny < 0 || nx < 0 || ny >= C || nx >= R || mtx[nx][ny] == -1) {
+                continue;
+              }
+              tmp[nx][ny] += spread;
+              cnt++;
+            }
+            mtx[i][j] = mtx[i][j] - (spread * cnt);
+          }
+        }
+      }
+
+      for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+          mtx[i][j] += tmp[i][j];
+        }
+      }
+    }
+
+    private List<Point> chung(int x, int y, int[] dx, int[] dy) {
+      int orX = x;
+      int orY = y;
+      int dir = 0;
+      int nx, ny;
+      List<Point> points = new ArrayList<>();
+      while (true) {
+        nx = x + dx[dir];
+        ny = y + dy[dir];
+        if (nx == orX && ny == orY) {
+          break;
+        }
+        if (ny >= C || nx < 0 || ny < 0 || nx >= R) {
+          dir++;
+          nx = x + dx[dir];
+          ny = y + dy[dir];
+        }
+        points.add(new Point(nx, ny));
+        x = nx;
+        y = ny;
+      }
+      return points;
+    }
+  }
+
+
+  // #########################################################
+  // #########################################################
+  // #########################################################
+  // #########################################################
 
   static class SolutionImplV2 implements Solution {
     static int[][] a, temp;
