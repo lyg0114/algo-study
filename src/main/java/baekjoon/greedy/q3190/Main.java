c643bcdf22d301942a284b3260a865240d2d49af
lyg0114 <whdnseowkd@naver.com>
2024-01-09 22:31:16 +0100

    백준 : 뱀 - 3190 - 오답 Question3190_V1

---
+++ src/main/java/baekjoon/greedy/q3190/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.greedy.q3190;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.greedy.q3190
+ * @since : 09.01.24
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/main/java/baekjoon/greedy/q3190/Solution.java
@@ -0,0 +1,13 @@
+package baekjoon.greedy.q3190;
+
+import java.io.IOException;
+
+/**
+ * @author : iyeong-gyo
+ * @package : baekjoon.greedy.q3190
+ * @since : 09.01.24
+ */
+public interface Solution {
+
+  void doMain() throws IOException;
+}
