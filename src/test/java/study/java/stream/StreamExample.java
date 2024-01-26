171aa5d6d9f0f24bcae02f391cf3834ea455c20f
lyg0114 <whdnseowkd@naver.com>
2024-01-26 08:40:18 +0100

    Add : StreamExample.java

--- src/test/java/study/java/stream/StreamExample.java
+++ src/test/java/study/java/stream/StreamExample.java
@@ -1,6 +1,7 @@
 package study.java.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.junit.Test;
 
@@ -32,5 +33,38 @@ public class StreamExample {
     ;
   }
 
+  @Test
+  public void test3() {
+    // 예제를 위한 데이터 리스트
+    List<String> fruits = List.of("Apple", "Banana", "Orange", "Grapes", "Mango");
+    Map<String, Integer> fruitLengthMap = fruits.stream()  // 스트림을 사용하여 각 과일과 그 길이를 매핑한 후 HashMap으로 변환
+        .collect(Collectors.toMap(
+            fruit -> fruit,       // 키 매핑: 과일 이름
+            String::length        // 값 매핑: 과일 이름의 길이
+        ));
 
+    System.out.println(fruitLengthMap);
+  }
+
+  @Test
+  public void extractMapByLastName() {
+    List<String> fruits = List.of(
+        "Apple Lee", "Banana Park", "Orange Lee", "Grapes Kim", "Mango Park",
+        "yglee", "gabeen"
+    );
+
+    Map<String, List<String>> result = fruits.stream()
+        .collect(Collectors.groupingBy(fruit -> {
+          String[] parts = fruit.split(" ");
+          if (parts.length > 1) {
+            return parts[1];
+          } else {
+            return "onlyName";
+          }
+        }));
+
+    result.forEach((lastName, names) -> {
+      System.out.println(lastName + ": " + names);
+    });
+  }
 }
