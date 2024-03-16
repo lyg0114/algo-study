247b1fc241fa1882fff416a5daa4219963cd831e
lyg0114 <whdnseowkd@naver.com>
2024-03-16 21:57:36 +0100

    test : add VitualThreadExample

---
+++ src/test/java/study/java/thread/VitualThreadExample.java
@@ -0,0 +1,27 @@
+package study.java.thread;
+
+import java.util.concurrent.ThreadFactory;
+import java.lang.Thread;
+import org.junit.Test;
+
+/**
+ * @author : iyeong-gyo
+ * @package : study.java.thread
+ * @since : 16.03.24
+ */
+public class VitualThreadExample {
+
+  @Test
+  public void test1() {
+    Runnable printThread = () -> System.out.println(Thread.currentThread());
+
+    ThreadFactory virtualThreadFactory = Thread.ofVirtual().factory();
+    ThreadFactory kernelThreadFactory = Thread.ofVirtual().factory();
+
+    Thread virtualThread = virtualThreadFactory.newThread(printThread);
+    Thread kernelThread = kernelThreadFactory.newThread(printThread);
+
+    virtualThread.start();
+    kernelThread.start();
+  }
+}
