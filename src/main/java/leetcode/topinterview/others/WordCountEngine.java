5829e94e2c9b215e649a540cbc42d70a7ebcaf70
lyg0114 <whdnseowkd@naver.com>
2024-04-13 12:01:00 +0200

    pramp : wordCountEngine 정답

--- src/main/java/leetcode/topinterview/others/WordCountEngine.java
+++ src/main/java/leetcode/topinterview/others/WordCountEngine.java
@@ -13,12 +13,16 @@ import java.util.stream.Collectors;
  * @since : 11.04.24
  */
 public class WordCountEngine {
-
   public static void main(String[] args) {
     String[][] strings
         = wordCountEngine(
         "Practice makes perfect. you'll only get Perfect by practice. just practice!"
     );
+    for (int i = 0; i < strings.length; i++) {
+      for (int j = 0; j < strings[i].length; j++) {
+        System.out.println("strings = " + strings[i][j]);
+      }
+    }
   }
 
   // sort by count des ordr
@@ -29,25 +33,22 @@ public class WordCountEngine {
   // 1. strip out punctuation [x]
   // 2. seperate sentenct by whitespace [x]
   // 3. iterate sentence and check the count and make the Map<Integer, List<String>>
-
   public static String[][] wordCountEngine(String document) {
     String[] split = document.toLowerCase().split(" ");
     for (int i = 0; i < split.length; i++) {
-      split[i] = split[i].replaceAll("[^a-z]", "");
-    }
-    Map<String, Long> collect = Arrays.stream(split)
-        .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
-    HashMap<Long, ArrayList<String>> results = new HashMap<>();
-
+      split[i] = split[i].replaceAll("[^a-z]", ""); }
+    Map<String, Long> collect = Arrays.stream(split).collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+    Map<Long, List<String>> results = new HashMap<>();
     long maxCnt = 0;
+
     for (String s : split) {
       Long key = collect.get(s);
       maxCnt = key > maxCnt ? key : maxCnt;
       List<String> strings = results.get(key);
       if (strings == null || strings.isEmpty()) {
-        ArrayList<String> objects = new ArrayList<>();
-        objects.add(s);
-        results.put(key, objects);
+        List<String> data = new ArrayList<>();
+        data.add(s);
+        results.put(key, data);
       } else {
         List<String> strs = results.get(key);
         if(!strs.contains(s)){
@@ -56,7 +57,19 @@ public class WordCountEngine {
       }
     }
 
-    System.out.println("results = " + results);
-    return null;
+    String [][] res = new String[collect.size()][2];
+    int idx = 0;
+    if(idx < collect.size() - 1){
+      for (long key = (int) maxCnt; key > 0; key--) {
+        List<String> strings = results.get(key);
+        for (String str : strings) {
+          res[idx][0] = str;
+          res[idx][1] = String.valueOf(key);
+          idx++;
+        }
+      }
+    }
+
+    return res;
   }
 }
